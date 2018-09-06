2b72683b9b246909f5a5d30751e1ebcc34bfdaff
detroyejr <detroyejr@outlook.com>
2018-09-06 15:51:16 -0400

    Add packages for pyenv

--- install.sh
+++ install.sh
@@ -10,5 +10,25 @@ vim -c PluginInstall
 ## Add PyEnv
 git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
 
+# Packages for pyenv.
+sudo apt-get install -y \
+    make \
+    build-essential \
+    libssl-dev \
+    zlib1g-dev \
+    libbz2-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    wget \
+    curl \
+    llvm \
+    libncurses5-dev \
+    xz-utils \
+    tk-dev \
+    libxml2-dev \
+    libxmlsec1-dev \
+    libffi-dev
+
+
 ## Add .bashrc file.
 cp -f bash/.bashrc $HOME/.bashrc
