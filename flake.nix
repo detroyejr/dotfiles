710241ea7fab77bcd1c5c72a665d5d05438017dc
detroyejr <detroyejr@outlook.com>
2023-02-05 00:34:44 +0000

    Add a separate home configuration nixos

--- flake.nix
+++ flake.nix
@@ -15,6 +15,7 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
+      # A default configuration that should work on non-NixOS machines.
       homeConfigurations.detroyejr = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
@@ -33,6 +34,25 @@
         # to pass through arguments to home.nix
       };
 
+      homeConfigurations.surface = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [
+          ./nix/home.nix
+          ./nix/extras.nix
+          ./nix/neovim.nix
+          ./nix/python.nix
+          ./nix/r.nix
+          ./nix/rust.nix
+          ./nix/git.nix
+        ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+      
       nixosConfigurations.detroyejr = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [ ./nixos/surface/configuration.nix ];
