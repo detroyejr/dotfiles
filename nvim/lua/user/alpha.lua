ebf21df042cdc04c99da2f7a7206e1876d0e8574
detroyejr <detroyejr@outlook.com>
2023-02-14 11:02:46 -0500

    Disable 'fortune' for now

--- nvim/lua/user/alpha.lua
+++ nvim/lua/user/alpha.lua
@@ -28,7 +28,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return "detroyejr@outlook.com"
+	-- return "detroyejr@outlook.com"
 end
 
 dashboard.section.footer.val = footer()
