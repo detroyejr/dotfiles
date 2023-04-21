1ba41705248bdaad6c7482ca6f177b1c2c3d7570
detroyejr <detroyejr@outlook.com>
2023-04-21 10:32:23 -0400

    feat(nix) import nixos config

--- flake.nix
+++ flake.nix
@@ -93,6 +93,23 @@
           }
         ];
       };
-    };
 
+      nixosConfigurations.brick = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ 
+          ./nixos/brick/configuration.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit devenv; };
+            home-manager.users.detroyejr = {
+              imports = [
+                ./nix/home.nix
+                ./nix/dev
+               ];
+            };
+          }
+        ];
+      };
+    };
 }
