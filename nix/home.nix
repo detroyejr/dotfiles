08eef1e78183dc1dae14daff484d75fa25efb8a2
detroyejr <detroyejr@outlook.com>
2023-05-08 10:49:24 -0400

    fix(bash): switch to tmux-new script instead of alias allowing arguments

--- nix/home.nix
+++ nix/home.nix
@@ -58,7 +58,6 @@
       st = "set-title";
       tf = "tmux-fzf";
       k = "kubectl";
-      tmux-new = "echo 'Enter session name:' && read name && tmux new -s $name";
     };
     plugins = [
       {
