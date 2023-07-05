810521e54eb7c8ac1fc97628d11ff4aa1f5b1778
detroyejr <detroyejr@outlook.com>
2023-07-05 15:41:49 -0400

    nix(feat): Add direnv

--- nix/home.nix
+++ nix/home.nix
@@ -184,5 +184,10 @@
           - "#python"
   '';
 
+  programs.direnv.enable = true;
+  programs.direnv.nix-direnv.enable = true;
+  # optional for nix flakes support in home-manager 21.11, not required in home-manager unstable or 22.05
+  programs.direnv.nix-direnv.enableFlakes = true;
+
   home.stateVersion = "22.11";
 }
