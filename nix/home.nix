0ab6e6f0cd598a0481ad3a36accb3758d5ae9a3c
detroyejr <detroyejr@outlook.com>
2023-07-05 15:44:01 -0400

    fix(nix): flake option not required

--- nix/home.nix
+++ nix/home.nix
@@ -186,8 +186,6 @@
 
   programs.direnv.enable = true;
   programs.direnv.nix-direnv.enable = true;
-  # optional for nix flakes support in home-manager 21.11, not required in home-manager unstable or 22.05
-  programs.direnv.nix-direnv.enableFlakes = true;
 
   home.stateVersion = "22.11";
 }
