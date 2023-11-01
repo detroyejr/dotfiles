c9dbb386ab3b39266670d12f73e6dee4ab4a99fe
detroyejr <detroyejr@outlook.com>
2023-11-01 16:58:18 -0400

    feat: cd-fzf script

--- nix/home.nix
+++ nix/home.nix
@@ -142,10 +142,6 @@
       set -g @plugin 'tmux-plugins/tmux-sensible'
       set -g @plugin 'tmux-plugins/tmux-yank'
       set -g @plugin 'tmux-plugins/tpm'
-
-      # Set status color manually for tokyo night theme.
-      # set -g status-bg "#1a1b26"
-      # set -g status-style bg=default
       set -g @themepack 'basic'
 
       run '$HOME/.tmux/plugins/tpm/tpm'
