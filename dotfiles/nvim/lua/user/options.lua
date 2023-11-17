5b8014f7cff78d4d7e3e7705d78ad15ea372b0ce
detroyejr <detroyejr@outlook.com>
2023-11-17 09:54:16 -0500

    feat: use the github repo for spellfiles if available

--- dotfiles/nvim/lua/user/options.lua
+++ dotfiles/nvim/lua/user/options.lua
@@ -1,64 +1,72 @@
 -- Toggleterm specific settings for powershell.
 
 if vim.fn.has("win32") == 1 then
-	local powershell_options = {
-		shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
-		shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-		shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-		shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-		shellquote = "",
-		shellxquote = "",
-	}
+  local powershell_options = {
+    shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+    shellcmdflag =
+    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+    shellquote = "",
+    shellxquote = "",
+  }
 
-	for option, value in pairs(powershell_options) do
-		vim.opt[option] = value
-	end
+  for option, value in pairs(powershell_options) do
+    vim.opt[option] = value
+  end
+end
+
+-- Location of the spellfile.
+SpellFile = os.getenv("HOME") .. "/.config/dotfiles/dotfiles/nvim/spell/en.utf-8.add"
+if not os.rename(SpellFile, SpellFile) then
+  SpellFile = vim.opt.spellfile
 end
 
 -- :help options
 local options = {
-	backup = false,
-	clipboard = "unnamedplus", -- Use the system clipboard.
-	cmdheight = 0,
-	colorcolumn = "88",
-	completeopt = { "menuone", "noselect" },
-	conceallevel = 0,
-	cursorline = false,
-	expandtab = true, -- tabs to spaces.
-	fileencoding = "utf-8",
-	fileformat = "unix",
-	guifont = "monospace:h17",
-	hlsearch = true,
-	ignorecase = true,
-	mouse = "a",
-	number = true,
-	numberwidth = 4,
-	pumheight = 10,
-	relativenumber = true,
-	scrolloff = 8,
-	shiftwidth = 2,
-	showmode = false,
-	showtabline = 2,
-	sidescrolloff = 8,
-	signcolumn = "yes",
-	smartcase = true,
-	smartindent = true,
-	spell = true,
-	spelllang = "en_us",
-	splitbelow = true,
-	splitright = true,
-	swapfile = false,
-	tabstop = 2,
-	termguicolors = true,
-	timeoutlen = 200,
-	undofile = true,
-	updatetime = 50,
-	wrap = false,
-	writebackup = false,
+  backup = false,
+  clipboard = "unnamedplus", -- Use the system clipboard.
+  cmdheight = 0,
+  colorcolumn = "88",
+  completeopt = { "menuone", "noselect" },
+  conceallevel = 0,
+  cursorline = false,
+  expandtab = true, -- tabs to spaces.
+  fileencoding = "utf-8",
+  fileformat = "unix",
+  guifont = "monospace:h17",
+  hlsearch = true,
+  ignorecase = true,
+  mouse = "a",
+  number = true,
+  numberwidth = 4,
+  pumheight = 10,
+  relativenumber = true,
+  scrolloff = 8,
+  shiftwidth = 2,
+  showmode = false,
+  showtabline = 2,
+  sidescrolloff = 8,
+  signcolumn = "yes",
+  smartcase = true,
+  smartindent = true,
+  spell = true,
+  spellfile = SpellFile,
+  spelllang = "en_us",
+  splitbelow = true,
+  splitright = true,
+  swapfile = false,
+  tabstop = 2,
+  termguicolors = true,
+  timeoutlen = 200,
+  undofile = true,
+  updatetime = 50,
+  wrap = false,
+  writebackup = false,
 }
 
 for k, v in pairs(options) do
-	vim.opt[k] = v
+  vim.opt[k] = v
 end
 
 vim.opt.shortmess:append("c")
