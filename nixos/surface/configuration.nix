9fc32c1963ca480dd8718f37054efa74e5480fd7
detroyejr <detroyejr@outlook.com>
2023-11-21 17:21:06 +0000

    chore: sign even more by default

--- nixos/surface/configuration.nix
+++ nixos/surface/configuration.nix
@@ -100,6 +100,7 @@
     path = [ pkgs.sbctl pkgs.gawk pkgs.util-linux ];
     script = ''
       sbctl verify | grep Microsoft/Boot/b | awk '{print $2}' | xargs -L1 sbctl sign
+      sbctl verify | grep Microsoft/Boot/en-US/b | awk '{print $2}' | xargs -L1 sbctl sign
       ls /boot/EFI/nixos | xargs -n1 -I {} sbctl sign /boot/EFI/nixos/{} | exit 0
     '';
     serviceConfig = {
