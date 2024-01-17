b89af7c560c83387b2459efa66c9064f59b1ad22
detroyejr <detroyejr@outlook.com>
2024-01-17 16:31:03 -0500

    chore: fix

--- nix/window/hyprland/default.nix
+++ nix/window/hyprland/default.nix
@@ -42,15 +42,9 @@ in
   };
 
   home.file.".config/hypr/hyprland.conf".text = with colorScheme.colors; ''
-        # change monitor to high resolution, the last argument is the scale factor
-        monitor=eDP-1,preferred,auto,2
-        monitor=,highres,auto,1
-
-<<<<<<< HEAD
-    # unscale XWayland
-    xwayland {
-      force_zero_scaling = true
-    }
+    # change monitor to high resolution, the last argument is the scale factor
+    monitor=eDP-1,preferred,auto,2
+    monitor=,highres,auto,1
 
     # toolkit-specific scale
     env = BROWSER,firefox;
@@ -65,11 +59,13 @@ in
     env = TERMINAL,kitty;
     env = XCURSOR_SIZE,32
 
-    exec-once=dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
-
     exec-once = hyprctl dispatch dpms on &
     exec-once = kanshi & swaylock & hyprpaper & dunst & eww open bar &
     exec-once = hyprctl set-cursor Numix-Cursor 24
+
+    windowrule = float, ^(Rofi)$
+    windowrule = center,^(Rofi)$
+    windowrule = animation popin 0,^(Rofi)$
 
     windowrule = float,^(thunar)$
     windowrule = center,^(thunar)$
@@ -107,32 +103,11 @@ in
 
         touchpad {
             natural_scroll = yes
-            scroll_factor = 0.4
-=======
-        # unscale XWayland
-        xwayland {
-          force_zero_scaling = true
->>>>>>> b9fc71c (chore: cleanup ranger)
+            scroll_factor = 0.5
         }
 
-        # toolkit-specific scale
-        env = BROWSER,firefox;
-        env = EDITOR,nvim;
-        env = GDK_SCALE,2
-        env = MOZ_ENABLE_WAYLAND,1;
-        env = NIXOS_OZONE_WL,1
-        env = QT_QPA_PLATFORM,wayland;xcb;
-        env = QT_QPA_PLATFORMTHEME,qt5ct;
-        env = QT_AUTO_SCALE_SCREEN_FACTOR,1
-        env = QT_WAYLAND_DISABLE_WINDOWDECORATION,1
-        env = TERMINAL,kitty;
-        env = XCURSOR_SIZE,32
-
-<<<<<<< HEAD
-    device:steelseries-steelseries-sensei-310-esports-mouse {
-        sensitivity = -0.7
+        sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
     }
-
 
     general {
         gaps_in = 4
@@ -142,212 +117,148 @@ in
         col.inactive_border = rgb(${base00})
         layout = dwindle
     }
-=======
-        exec-once=dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
->>>>>>> b9fc71c (chore: cleanup ranger)
 
-        exec-once = hyprctl dispatch dpms on &
-        exec-once = swaylock & hyprpaper & dunst & eww open bar &
-        exec-once = kanshi &
-        exec-once = hyprctl set-cursor Numix-Cursor 24
+    decoration {
+        rounding = 5
+        drop_shadow = yes
+        shadow_range = 4
+        shadow_render_power = 3
+        col.shadow = rgba(${base07}ee)
+        dim_inactive = false
+    }
 
+    animations {
+        enabled = yes
 
-        windowrule = float, ^(Rofi)$
-        windowrule = center,^(Rofi)$
-        windowrule = animation popin 0,^(Rofi)$
+        # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
 
-        windowrule = float,^(thunar)$
-        windowrule = center,^(thunar)$
-        windowrule = size 70% 70%,^(thunar)$
+        bezier = myBezier, 0.05, 0.9, 0.1, 1.05
 
-        windowrule = float,^(pavucontrol)$
-        windowrule = move 100%-488 6%,^(pavucontrol)$
-        windowrule = size 400 560,^(pavucontrol)$
-        windowrule = animation slide,^(pavucontrol)$
+        animation = windows, 1, 7, myBezier
+        animation = windowsOut, 1, 7, default, popin 80%
+        animation = border, 1, 10, default
+        animation = borderangle, 1, 8, default
+        animation = fade, 1, 7, default
+        animation = workspaces, 1, 6, default
+    }
 
-        windowrule = float,^(Plexamp)$
-        windowrule = move 100%-488 6%,title:^(Plexamp)$
-        windowrule = size 400 560,title:^(Plexamp)$
-        windowrule = animation slide,^(Plexamp)$
+    binds {
+        allow_workspace_cycles = true
+    }
 
-        windowrule = float,title:^(nmtui-connect)$
-        windowrule = move 100%-488 6%,title:^(nmtui-connect)$
-        windowrule = size 100 160,^(nmtui-connect)$
-        windowrule = animation slide,title:^(nmtui-connect)$
+    dwindle {
+        # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
+        pseudotile = yes # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+        preserve_split = yes # you probably want this
+    }
 
-        windowrule = float,title:(.*)(- KeePassXC)$
-        windowrule = center,title:(.*)(- KeePassXC)$
-        windowrule = animation popin,title:(.*)(- KeePassXC)$
-        windowrule = size 70% 70%,title:(.*)(- KeePassXC)$
-    ,
-        # For all categories, see https://wiki.hyprland.org/Configuring/Variables/
-        input {
-            kb_layout = us
-            kb_variant =
-            kb_model =
-            kb_options =
-            kb_rules =
+    master {
+        # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
+        new_is_master = true
+    }
 
-            follow_mouse = 1
+    gestures {
+        # See https://wiki.hyprland.org/Configuring/Variables/ for more
+        workspace_swipe = off
+    }
 
-            touchpad {
-                natural_scroll = yes
-                scroll_factor = 0.5
-            }
+    misc {
+      disable_hyprland_logo = true
+    }
 
-            sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
-        }
-
-        general {
-            gaps_in = 4
-            gaps_out = 8
-            border_size = 2
-            col.active_border = rgba(${base07}66)
-            col.inactive_border = rgb(${base00})
-            layout = dwindle
-        }
-
-        decoration {
-            rounding = 5
-            drop_shadow = yes
-            shadow_range = 4
-            shadow_render_power = 3
-            col.shadow = rgba(${base07}ee)
-            dim_inactive = false
-        }
-
-        animations {
-            enabled = yes
-
-            # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
-
-            bezier = myBezier, 0.05, 0.9, 0.1, 1.05
-
-            animation = windows, 1, 7, myBezier
-            animation = windowsOut, 1, 7, default, popin 80%
-            animation = border, 1, 10, default
-            animation = borderangle, 1, 8, default
-            animation = fade, 1, 7, default
-            animation = workspaces, 1, 6, default
-        }
-
-        binds {
-            allow_workspace_cycles = true
-        }
-
-        dwindle {
-            # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
-            pseudotile = yes # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-            preserve_split = yes # you probably want this
-        }
-
-        master {
-            # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
-            new_is_master = true
-        }
-
-        gestures {
-            # See https://wiki.hyprland.org/Configuring/Variables/ for more
-            workspace_swipe = off
-        }
-
-        misc {
-          disable_hyprland_logo = true
-        }
-
-        device:epic-mouse-v1 {
-            sensitivity = 0
-        }
+    device:steelseries-steelseries-sensei-310-esports-mouse {
+        sensitivity = -0.7
+    }
 
 
-        # See https://wiki.hyprland.org/Configuring/Keywords/ for more
-        $mainMod = SUPER
+    # See https://wiki.hyprland.org/Configuring/Keywords/ for more
+    $mainMod = SUPER
 
-        # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
-        bind = $mainMod CTRL, S, exec, grim -g "$(slurp)"
-        bind = $mainMod CTRL, P, exec, hyprpicker | wl-copy
-        bind = $mainMod SHIFT, J, exec, bash -c "if pgrep -x @joplinapp-desk > /dev/null; then kill $(pgrep -x  @joplinapp-desk | paste -sd ' '); else joplin-desktop; fi"
-        bind = $mainMod SHIFT, K, exec, bash -c "if pgrep -x .keepassxc-wrap > /dev/null; then kill $(pgrep -x .keepassxc-wrap); else keepassxc; fi"
-        bind = $mainMod, C, killactive,
-        bind = $mainMod, E, exec, thunar
-        bind = $mainMod, F, togglefloating,
-        bind = $mainMod, F, centerwindow
-        bind = $mainMod, F, resizeactive, exact 1000 800
-        bind = $mainMod, G, fullscreen
-        bind = $mainMod, J, togglesplit, # dwindle
-        bind = $mainMod, L, exec, bash -c "swaylock"
-        bind = $mainMod, P, pseudo, # dwindle
-        bind = $mainMod, Q, exec, kitty
-        bind = $mainMod SHIFT, R, exec, bash -c "if pgrep -x rofi > /dev/null; then kill $(pgrep -x rofi); else rofi -show run; fi"
-        bind = $mainMod, R, exec, bash -c "if pgrep -x rofi > /dev/null; then kill $(pgrep -x rofi); else rofi -show drun; fi"
-        bind = $mainMod, R, exec, hyprctl dispatch focuswindow ^(Rofi)$
-        bind = CTRL ALT, Delete, exit
+    # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
+    bind = $mainMod CTRL, S, exec, grim -g "$(slurp)"
+    bind = $mainMod CTRL, P, exec, hyprpicker | wl-copy
+    bind = $mainMod SHIFT, J, exec, bash -c "if pgrep -x @joplinapp-desk > /dev/null; then kill $(pgrep -x  @joplinapp-desk | paste -sd ' '); else joplin-desktop; fi"
+    bind = $mainMod SHIFT, K, exec, bash -c "if pgrep -x .keepassxc-wrap > /dev/null; then kill $(pgrep -x .keepassxc-wrap); else keepassxc; fi"
+    bind = $mainMod, C, killactive,
+    bind = $mainMod, E, exec, thunar
+    bind = $mainMod, F, togglefloating,
+    bind = $mainMod, F, centerwindow
+    bind = $mainMod, F, resizeactive, exact 1000 800
+    bind = $mainMod, G, fullscreen
+    bind = $mainMod, J, togglesplit, # dwindle
+    bind = $mainMod, L, exec, bash -c "swaylock"
+    bind = $mainMod, P, pseudo, # dwindle
+    bind = $mainMod, Q, exec, kitty
+    bind = $mainMod SHIFT, R, exec, bash -c "if pgrep -x rofi > /dev/null; then kill $(pgrep -x rofi); else rofi -show run; fi"
+    bind = $mainMod, R, exec, bash -c "if pgrep -x rofi > /dev/null; then kill $(pgrep -x rofi); else rofi -show drun; fi"
+    bind = $mainMod, R, exec, hyprctl dispatch focuswindow ^(Rofi)$
+    bind = CTRL ALT, Delete, exit
 
-        # Move focus with mainMod + arrow keys
-        bind = $mainMod, left, movefocus, l
-        bind = $mainMod, right, movefocus, r
-        bind = $mainMod, up, movefocus, u
-        bind = $mainMod, down, movefocus, d
+    # Move focus with mainMod + arrow keys
+    bind = $mainMod, left, movefocus, l
+    bind = $mainMod, right, movefocus, r
+    bind = $mainMod, up, movefocus, u
+    bind = $mainMod, down, movefocus, d
 
-        bind = $mainMod CTRL, h, movefocus, l
-        bind = $mainMod CTRL, l, movefocus, r
-        bind = $mainMod CTRL, k, movefocus, u
-        bind = $mainMod CTRL, j, movefocus, d
-        # Switch workspaces with mainMod + [0-9]
-        bind = $mainMod, 1, workspace, 1
-        bind = $mainMod, 2, workspace, 2
-        bind = $mainMod, 3, workspace, 3
-        bind = $mainMod, 4, workspace, 4
-        bind = $mainMod, 5, workspace, 5
-        bind = $mainMod, 6, workspace, 6
-        bind = $mainMod, 7, workspace, 7
-        bind = $mainMod, 8, workspace, 8
-        bind = $mainMod, 9, workspace, 9
-        bind = $mainMod, 0, workspace, 10
+    bind = $mainMod CTRL, h, movefocus, l
+    bind = $mainMod CTRL, l, movefocus, r
+    bind = $mainMod CTRL, k, movefocus, u
+    bind = $mainMod CTRL, j, movefocus, d
+    # Switch workspaces with mainMod + [0-9]
+    bind = $mainMod, 1, workspace, 1
+    bind = $mainMod, 2, workspace, 2
+    bind = $mainMod, 3, workspace, 3
+    bind = $mainMod, 4, workspace, 4
+    bind = $mainMod, 5, workspace, 5
+    bind = $mainMod, 6, workspace, 6
+    bind = $mainMod, 7, workspace, 7
+    bind = $mainMod, 8, workspace, 8
+    bind = $mainMod, 9, workspace, 9
+    bind = $mainMod, 0, workspace, 10
 
-        # Window
-        binde = $mainMod CTRL, up,    resizeactive, 0 -20
-        binde = $mainMod CTRL, down,  resizeactive, 0 20
-        binde = $mainMod CTRL, right, resizeactive, 20 0
-        binde = $mainMod CTRL, left,  resizeactive, -20 0
-        binde = $mainMod ALT,  up,    moveactive, 0 -20
-        binde = $mainMod ALT,  down,  moveactive, 0 20
-        binde = $mainMod ALT,  right, moveactive, 20 0
-        binde = $mainMod ALT,  left,  moveactive, -20 0
-        # Move active window to a workspace with mainMod + SHIFT + [0-9]
-        bind = $mainMod SHIFT, 1, movetoworkspace, 1
-        bind = $mainMod SHIFT, 2, movetoworkspace, 2
-        bind = $mainMod SHIFT, 3, movetoworkspace, 3
-        bind = $mainMod SHIFT, 4, movetoworkspace, 4
-        bind = $mainMod SHIFT, 5, movetoworkspace, 5
-        bind = $mainMod SHIFT, 6, movetoworkspace, 6
-        bind = $mainMod SHIFT, 7, movetoworkspace, 7
-        bind = $mainMod SHIFT, 8, movetoworkspace, 8
-        bind = $mainMod SHIFT, 9, movetoworkspace, 9
-        bind = $mainMod SHIFT, 0, movetoworkspace, 10
+    # Window
+    binde = $mainMod CTRL, up,    resizeactive, 0 -20
+    binde = $mainMod CTRL, down,  resizeactive, 0 20
+    binde = $mainMod CTRL, right, resizeactive, 20 0
+    binde = $mainMod CTRL, left,  resizeactive, -20 0
+    binde = $mainMod ALT,  up,    moveactive, 0 -20
+    binde = $mainMod ALT,  down,  moveactive, 0 20
+    binde = $mainMod ALT,  right, moveactive, 20 0
+    binde = $mainMod ALT,  left,  moveactive, -20 0
+    # Move active window to a workspace with mainMod + SHIFT + [0-9]
+    bind = $mainMod SHIFT, 1, movetoworkspace, 1
+    bind = $mainMod SHIFT, 2, movetoworkspace, 2
+    bind = $mainMod SHIFT, 3, movetoworkspace, 3
+    bind = $mainMod SHIFT, 4, movetoworkspace, 4
+    bind = $mainMod SHIFT, 5, movetoworkspace, 5
+    bind = $mainMod SHIFT, 6, movetoworkspace, 6
+    bind = $mainMod SHIFT, 7, movetoworkspace, 7
+    bind = $mainMod SHIFT, 8, movetoworkspace, 8
+    bind = $mainMod SHIFT, 9, movetoworkspace, 9
+    bind = $mainMod SHIFT, 0, movetoworkspace, 10
 
-        # Minimize
-        bind = $mainMod, m, exec, $HOME/.local/bin/bash/minimize
+    # Minimize
+    bind = $mainMod, m, exec, $HOME/.local/bin/bash/minimize
 
-        # Scroll through existing workspaces with mainMod + scroll
-        bind = $mainMod, mouse_down, workspace, e+1
-        bind = $mainMod, mouse_up, workspace, e-1
+    # Scroll through existing workspaces with mainMod + scroll
+    bind = $mainMod, mouse_down, workspace, e+1
+    bind = $mainMod, mouse_up, workspace, e-1
 
-        # Move/resize windows with mainMod + LMB/RMB and dragging
-        bindm = $mainMod, mouse:272, movewindow
-        bindm = $mainMod, mouse:273, resizewindow
+    # Move/resize windows with mainMod + LMB/RMB and dragging
+    bindm = $mainMod, mouse:272, movewindow
+    bindm = $mainMod, mouse:273, resizewindow
 
-        # Laptop
-        bindl = , XF86MonBrightnessUp,     exec, brightnessctl set "10%+"
-        bindl = , XF86MonBrightnessDown,   exec, brightnessctl set "10%-"
-        bindl = , XF86AudioRaiseVolume,    exec, pamixer --increase 10 --set-limit 100
-        bindl = , XF86AudioLowerVolume,    exec, pamixer --decrease 10
-        bindl = , XF86AudioMute,           exec, pamixer --toggle-mute
-        bindl  = , XF86AudioStop,           exec, playerctl stop
-        bindl  = , XF86AudioPause,          exec, playerctl pause
-        bindl  = , XF86AudioPrev,           exec, playerctl previous
-        bindl  = , XF86AudioNext,           exec, playerctl next
-        bindl  = , XF86AudioPlay,           exec, playerctl play-pause
+    # Laptop
+    bindl = , XF86MonBrightnessUp,     exec, brightnessctl set "10%+"
+    bindl = , XF86MonBrightnessDown,   exec, brightnessctl set "10%-"
+    bindl = , XF86AudioRaiseVolume,    exec, pamixer --increase 10 --set-limit 100
+    bindl = , XF86AudioLowerVolume,    exec, pamixer --decrease 10
+    bindl = , XF86AudioMute,           exec, pamixer --toggle-mute
+    bindl  = , XF86AudioStop,           exec, playerctl stop
+    bindl  = , XF86AudioPause,          exec, playerctl pause
+    bindl  = , XF86AudioPrev,           exec, playerctl previous
+    bindl  = , XF86AudioNext,           exec, playerctl next
+    bindl  = , XF86AudioPlay,           exec, playerctl play-pause
   '';
 
   home.file.".config/hypr/hyprpaper.conf".text = ''
