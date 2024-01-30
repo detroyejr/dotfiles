ad798b604e6f31258a65a8f5c60a7e593f2ce135
detroyejr <detroyejr@outlook.com>
2024-01-29 19:16:13 -0500

    rofi: trim the number of home.file calls and learn about nix maps in the process

--- nix/window/hyprland/rofi.nix
+++ nix/window/hyprland/rofi.nix
@@ -1,27 +1,51 @@
 { config, pkgs, colorScheme, ... }:
-{
+let
+  rofi-themes = pkgs.fetchFromGitHub {
+    owner = "adi1090x";
+    repo = "rofi";
+    rev = "master";
+    sha256 = "sha256-YjyrxappcLDoh3++mtZqCyxQV2qeoNhhUy2XGwlyTng=";
+  };
+  paths = [
+    "applets"
+    "colors"
+    "config.rasi"
+    "applets"
+    "config.rasi"
+    "launchers"
+    "scripts"
+    "colors"
+    "powermenu"
+  ];
+  originalRofiThemes = pkgs.lib.listToAttrs (map (e: pkgs.lib.nameValuePair ".config/rofi/${e}" { source = rofi-themes + "/files/" + e; }) paths);
+  customRofiTweaks = {
+    ".config/rofi/images/c.png" = {
+      source = builtins.fetchurl {
+        url = "https://raw.githubusercontent.com/detroyejr/dotfiles/main/assets/walls/minimal/minimal-12.jpg";
+        sha256 = "sha256:0p5k62dkxbwv90qdfnnmzxmsm3g6ix8b2dahdsvm46p8sa40abcb";
+      };
+    };
+    ".local/bin/rofi-launcher" = {
+      text = ''
+        dir="$HOME/.config/rofi/launchers/type-7"
+        theme='style-3'
 
+        ## Run
+        rofi \
+          -show drun \
+          -theme ''${dir}/''${theme}.rasi
+      '';
+      executable = true;
+    };
+  };
+  patchedRofiTheme = originalRofiThemes // customRofiTweaks;
+in
+{
 
   home.packages = with pkgs; [
     rofi
     rofi-power-menu
   ];
 
-  home.file.".config/rofi".source = pkgs.fetchFromGitHub
-    {
-      owner = "adi1090x";
-      repo = "rofi";
-      rev = "master";
-      sha256 = "sha256-YjyrxappcLDoh3++mtZqCyxQV2qeoNhhUy2XGwlyTng=";
-    } + "/files";
-
-  home.file.".local/usr/bin/rofi-launch".text = ''
-    dir="$HOME/.config/rofi/launchers/type-7"
-    theme='style-2'
-
-    ## Run
-    rofi \
-      -show drun \
-      -theme ''${dir}/''${theme}.rasi
-  '';
+  home.file = patchedRofiTheme;
 }
