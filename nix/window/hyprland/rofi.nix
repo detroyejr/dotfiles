1bd9aa21c780ec384cc883bec92b129f7815cbea
detroyejr <detroyejr@outlook.com>
2024-02-01 14:57:57 -0500

    rofi: figured out the right way to patch a git repo.
    Use patchPhase with find and sed to replace
    all instances of  keywords in a rasi file.

--- nix/window/hyprland/rofi.nix
+++ nix/window/hyprland/rofi.nix
@@ -1,39 +1,29 @@
 { config, pkgs, colorScheme, wallpaper, ... }:
 let
-  rofi-themes = pkgs.fetchFromGitHub {
-    owner = "adi1090x";
-    repo = "rofi";
-    rev = "master";
-    sha256 = "sha256-YjyrxappcLDoh3++mtZqCyxQV2qeoNhhUy2XGwlyTng=";
-  };
-  paths = [
-    "applets"
-    "colors"
-    "config.rasi"
-    "applets"
-    "config.rasi"
-    "launchers"
-    "scripts"
-    "colors"
-    "powermenu"
-  ];
-  originalRofiThemes = pkgs.lib.listToAttrs (map (e: pkgs.lib.nameValuePair ".config/rofi/${e}" { source = rofi-themes + "/files/" + e; }) paths);
-  customRofiTweaks = {
-    ".config/rofi/images/c.png".source = builtins.path { path = wallpaper; };
-    ".local/bin/rofi-launcher" = {
-      text = ''
-        dir="$HOME/.config/rofi/launchers/type-7"
-        theme='style-3'
-
-        ## Run
-        rofi \
-          -show drun \
-          -theme ''${dir}/''${theme}.rasi
-      '';
-      executable = true;
+  rofi-themes = pkgs.stdenv.mkDerivation {
+    name = "rofi-themes";
+    src = pkgs.fetchFromGitHub {
+      owner = "adi1090x";
+      repo = "rofi";
+      rev = "master";
+      sha256 = "sha256-YjyrxappcLDoh3++mtZqCyxQV2qeoNhhUy2XGwlyTng=";
     };
+    installPhase = ''
+      cp -r . $out;
+    '';
+    patchPhase = with colorScheme.colors; ''
+      find files -type f -name "*.rasi" -exec sed -i 's/font:.*$/font: "BlexMono Nerd Font Mono 12";/g' {} ';';
+      find files -type f -name "*.rasi" -exec sed -i 's/background:.*$/background: #${base00};/g' {} ';';
+      find files -type f -name "*.rasi" -exec sed -i 's/background-alt:.*$/background-alt: #${base01};/g' {} ';';
+      find files -type f -name "*.rasi" -exec sed -i 's/foreground:.*$/foreground: #${base06};/g' {} ';';
+      find files -type f -name "*.rasi" -exec sed -i 's/selected:.*$/selected: #${base02};/g' {} ';';
+      find files -type f -name "*.rasi" -exec sed -i 's/active:.*$/active: #${base04};/g' {} ';';
+      find files -type f -name "*.rasi" -exec sed -i 's/urgent:.*$/urgent: #${base01};/g' {} ';';
+      find files -type f -name "*.rasi" -exec sed -i 's/[a-j].png/wallpaper.png/g' {} ';';
+      find files -type f -name "*.rasi" -exec sed -i 's/[a-j].jpg/wallpaper.png/g' {} ';';
+      cp ${wallpaper} files/images/wallpaper.png
+    '';
   };
-  patchedRofiTheme = originalRofiThemes // customRofiTweaks;
 in
 {
 
@@ -42,5 +32,17 @@ in
     rofi-power-menu
   ];
 
-  home.file = patchedRofiTheme;
+  home.file.".config/rofi/".source = rofi-themes + "/files";
+  home.file.".local/bin/rofi-launcher" = {
+    text = ''
+      dir="$HOME/.config/rofi/launchers/type-7"
+      theme='style-8'
+
+      ## Run
+      rofi \
+        -show drun \
+        -theme ''${dir}/''${theme}.rasi
+    '';
+    executable = true;
+  };
 }
