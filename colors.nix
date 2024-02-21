01f29b7ec8916976c8bb2994f3f90c69eda38442
detroyejr <detroyejr@outlook.com>
2024-02-21 13:28:58 -0500

    colors: new gruvbox wallpaper

--- colors.nix
+++ colors.nix
@@ -28,7 +28,7 @@
   };
   gruvbox = {
     colorSchemeName = "gruvbox";
-    wallpaper = ./assets/walls/gruvbox/nathan-dumlao-tA90pRfL2gM-unsplash.jpg;
+    wallpaper = ./assets/walls/gruvbox/typewriter_armin_djuhic.jpg;
     colorScheme = {
       author = "Jonathan De Troye";
       name = "Gruvbox";
