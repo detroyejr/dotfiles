ac8ce4d1f800ab8b96579533dc39a291b67290de
detroyejr <detroyejr@outlook.com>
2024-02-23 10:40:42 -0500

    neovim: netrw open files in the same window

--- dotfiles/nvim/lua/user/options.lua
+++ dotfiles/nvim/lua/user/options.lua
@@ -62,7 +62,7 @@ vim.opt.shortmess:append("c")
 -- NetRW Options
 vim.g.netrw_altv = 1
 vim.g.netrw_banner = 0
-vim.g.netrw_browse_split = 4
+vim.g.netrw_browse_split = 0
 vim.g.netrw_liststyle = 3
 vim.g.netrw_preview = 1
 vim.g.netrw_winsize = 30
