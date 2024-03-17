3cd9c540c73c7185dc2f8f413d19edde68c8fd7f
detroyejr <detroyejr@outlook.com>
2024-03-17 16:27:09 -0400

    feat(hyprlock): add a rounded profile picture

--- nix/window/hyprland/hyprlock.nix
+++ nix/window/hyprland/hyprlock.nix
@@ -13,19 +13,32 @@
         {
           monitor = "";
           path = "${builtins.toString wallpaper}";
+          blur_passes = 1; # 0 disables blurring
+          blur_size = 7;
+        }
+      ];
+      images = [
+        {
+          monitor = "";
+          path = builtins.toString ../../../assets/profile.png;
+          position = { x = 0; y = 300; };
+          halign = "center";
+          valign = "center";
+          size = 400;
+          rounding = -2;
         }
       ];
       input-fields = [
         {
           monitor = "";
           outline_thickness = 2;
-          size = {width = 350; height = 90; };
-          dots_size = 0.1;
-          dots_spacing = 0.3;
+          size = { width = 350; height = 90; };
+          dots_size = 0.3;
+          dots_spacing = 0.4;
           outer_color = entry_border_color;
           inner_color = entry_background_color;
           font_color = entry_color;
-          position = { x = 0; y = -75; };
+          position = { x = 0; y = -100; };
           halign = "center";
           valign = "center";
         }
@@ -37,12 +50,13 @@
           color = text_color;
           font_size = 65;
           font_family = "BlexMono Nerd Font";
-          position = { x = 0; y = 300; };
+          position = { x = 0; y = 600; };
           halign = "center";
           valign = "center";
         }
         {
           text = "Hi there, $USER";
+          color = text_color;
           font_size = 35;
           position = { x = 0; y = 50; };
           font_family = "BlexMono Nerd Font";
