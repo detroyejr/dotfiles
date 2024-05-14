2f895d463d51b174303cddc3454fb3c6173e104f
detroyejr <detroyejr@outlook.com>
2024-05-13 23:23:49 -0400

    chore(cdda): bump

--- nix/cataclysm-dda.nix
+++ nix/cataclysm-dda.nix
@@ -13,9 +13,9 @@ let
   };
 
   base-cdda = pkgs.cataclysm-dda-git.override {
-    version = "2024-03-02";
-    rev = "7ee6c6a6a42596eba935fdb7270a801bb2e1b314";
-    sha256 = "sha256-z7wSI328J8FsYr/BcOYkLaTeeOLbme76AaSZz/JkWlo=";
+    version = "2024-05-12";
+    rev = "39e2afa80d50b06c8e3d13bdb6fc8dbeb23a1a63";
+    sha256 = "sha256-Y378fczn/YKxdyW18c193OvvBil7xt9GV1SH0QRk5Q0=";
   };
 
   cdda-no-mod = base-cdda.overrideAttrs (super: {
@@ -40,12 +40,21 @@ let
           hash = "sha256-X0da9cs60sr5jq4TPTMkNQAHAjcu1gPagJyLDJ7HOe0=";
         };
       };
+      tileset.UndeadPeopleCustom = cddaLib.buildTileSet {
+        modName = "UndeadPeopleCustom";
+        version = "March Release 1";
+        src = pkgs.fetchzip {
+          url = "https://github.com/Theawesomeboophis/UndeadPeopleTileset/releases/download/3%2F4%2F24/Sprinkles.zip";
+          hash = "sha256-5X8jvxIe6FW/PG5k5TvuGenlUU66FFft6TyjAeCq7ts=";
+          stripRoot = false;
+        };
+      };
     };
 
   cdda = (cddaLib.attachPkgs cddaLib.pkgs cdda-no-mod).withMods (mods:
     with mods.extend customMods; [
       soundpack.CC-Sounds
-      tileset.UndeadPeople
+      tileset.UndeadPeopleCustom
     ]);
 
   cdda-desktop = pkgs.makeDesktopItem {
