83ebd54240c8efc2c20268ccde8d15d863d9fd6b
detroyejr <detroyejr@outlook.com>
2024-06-17 10:51:15 -0400

    feat(joplin): remove from config

--- nix/window/hyprland/default.nix
+++ nix/window/hyprland/default.nix
@@ -182,6 +182,7 @@ in
     # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
     bind = $mainMod CTRL, S, exec, grim -g "$(slurp)"
     bind = $mainMod CTRL, P, exec, hyprpicker | wl-copy
+    # bind = $mainMod SHIFT, J, exec, bash -c "if pgrep -x @joplinapp-desk > /dev/null; then kill $(pgrep -x  @joplinapp-desk | paste -sd ' '); else joplin-desktop; fi"
     bind = $mainMod SHIFT, J, exec, bash -c "if pgrep -x @obsidian > /dev/null; then pkill obsidian; else obsidian; fi"
     bind = $mainMod SHIFT, K, exec, bash -c "if pgrep -x .keepassxc-wrap > /dev/null; then pkill keepassxc; else keepassxc; fi"
     bind = $mainMod SHIFT, E, exec, bash -c "if pgrep -x wofi-emoji > /dev/null; then pkill wofi-emoji; else wofi-emoji; fi"
