f97410e5add78f033ea862bbffa37c06ce2a38b2
detroyejr <detroyejr@outlook.com>
2024-06-19 09:23:35 -0400

    chore(flake): restructure

--- flake.nix
+++ flake.nix
@@ -36,6 +36,13 @@
     colorSchemeName = "rose-pine";
     colorScheme = nix-colors.${colorSchemeName}.colorScheme;
     wallpaper = nix-colors.${colorSchemeName}.wallpaper;
+
+    # Add custom quotes.
+    hyprland-patched = hyprland.packages.${system}.default.overrideAttrs (oldAttrs: {
+      prePatch = ''
+        cp ${./dotfiles/hyprland/Splashes.hpp} ./src/helpers/Splashes.hpp
+      '';
+    });
   in {
     formatter.${system} = nixpkgs.legacyPackages.${system}.alejandra;
 
@@ -52,148 +59,128 @@
       ];
     };
 
-    nixosConfigurations.xps = nixpkgs.lib.nixosSystem {
-      inherit pkgs;
-      system = "x86_64-linux";
-      modules = [
-        nixos-hardware.nixosModules.dell-xps-15-9520
-        nixos-hardware.nixosModules.dell-xps-15-9520-nvidia
-        ./nixos/xps/configuration.nix
-        # hyprland.nixosModules.default
-        # FIXME: Switching to this instead of using hyprland.nixosModules.default
-        # so that I can modify hyprland.packages.${system}.default without having
-        # to pass hyprland itself through.
-        {
-          programs.hyprland = {
-            enable = true;
-            package = hyprland.packages.${system}.default.overrideAttrs (oldAttrs: {
-              prePatch = ''
-                cp ${./dotfiles/hyprland/Splashes.hpp} ./src/helpers/Splashes.hpp
-              '';
-            });
-            xwayland = {
+    nixosConfigurations = {
+      "xps" = nixpkgs.lib.nixosSystem {
+        inherit system pkgs;
+        modules = [
+          nixos-hardware.nixosModules.dell-xps-15-9520
+          nixos-hardware.nixosModules.dell-xps-15-9520-nvidia
+          ./nixos/xps/configuration.nix
+          {
+            programs.hyprland = {
               enable = true;
+              package = hyprland-patched;
+              xwayland = {
+                enable = true;
+              };
             };
-          };
-        }
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.extraSpecialArgs = {inherit nix-colors colorSchemeName colorScheme wallpaper hyprland;};
-          home-manager.users.detroyejr = {
-            imports = [
-              ./nix/home.nix
-              ./nix/dev
-              ./nix/window/hyprland
-              ./nix/cataclysm-dda.nix
-            ];
-          };
-        }
-      ];
-    };
-
-    nixosConfigurations.mini = nixpkgs.lib.nixosSystem {
-      inherit pkgs;
-      system = "x86_64-linux";
-      modules = [
-        ./nixos/mini/configuration.nix
-        # hyprland.nixosModules.default
-        # FIXME: Switching to this instead of using hyprland.nixosModules.default
-        # so that I can modify hyprland.packages.${system}.default without having
-        # to pass hyprland itself through.
-        {
-          programs.hyprland = {
-            enable = true;
-            package = hyprland.packages.${system}.default.overrideAttrs (oldAttrs: {
-              prePatch = ''
-                cp ${./dotfiles/hyprland/Splashes.hpp} ./src/helpers/Splashes.hpp
-              '';
-            });
-            xwayland = {
+          }
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = {inherit nix-colors colorSchemeName colorScheme wallpaper hyprland;};
+            home-manager.users.detroyejr = {
+              imports = [
+                ./nix/home.nix
+                ./nix/dev
+                ./nix/window/hyprland
+                ./nix/cataclysm-dda.nix
+              ];
+            };
+          }
+        ];
+      };
+      "mini" = nixpkgs.lib.nixosSystem {
+        inherit system pkgs;
+        modules = [
+          ./nixos/mini/configuration.nix
+          {
+            programs.hyprland = {
               enable = true;
+              package = hyprland-patched;
+              xwayland = {
+                enable = true;
+              };
             };
-          };
-        }
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.extraSpecialArgs = {inherit nix-colors colorSchemeName colorScheme wallpaper hyprland;};
-          home-manager.users.detroyejr = {
-            imports = [
-              ./nix/home.nix
-              ./nix/dev
-              ./nix/window/hyprland
-              ./nix/cataclysm-dda.nix
-            ];
-          };
-        }
-      ];
-    };
+          }
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = {inherit nix-colors colorSchemeName colorScheme wallpaper hyprland;};
+            home-manager.users.detroyejr = {
+              imports = [
+                ./nix/home.nix
+                ./nix/dev
+                ./nix/window/hyprland
+                ./nix/cataclysm-dda.nix
+              ];
+            };
+          }
+        ];
+      };
 
-    nixosConfigurations.surface = nixpkgs.lib.nixosSystem {
-      inherit pkgs;
-      system = "x86_64-linux";
-      modules = [
-        nixos-hardware.nixosModules.microsoft-surface-pro-intel
-        ./nixos/surface/configuration.nix
-        hyprland.nixosModules.default
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.extraSpecialArgs = {inherit nix-colors colorSchemeName colorScheme wallpaper hyprland;};
-          home-manager.users.detroyejr = {
-            imports = [
-              ./nix/home.nix
-              ./nix/dev
-              ./nix/window/hyprland
-            ];
-          };
-        }
-      ];
-    };
+      "surface" = nixpkgs.lib.nixosSystem {
+        inherit system pkgs;
+        modules = [
+          nixos-hardware.nixosModules.microsoft-surface-pro-intel
+          ./nixos/surface/configuration.nix
+          hyprland.nixosModules.default
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = {inherit nix-colors colorSchemeName colorScheme wallpaper hyprland;};
+            home-manager.users.detroyejr = {
+              imports = [
+                ./nix/home.nix
+                ./nix/dev
+                ./nix/window/hyprland
+              ];
+            };
+          }
+        ];
+      };
 
-    nixosConfigurations.tower = nixpkgs.lib.nixosSystem {
-      inherit pkgs;
-      system = "x86_64-linux";
-      modules = [
-        ./nixos/tower/configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.detroyejr = {
-            imports = [
-              ./nix/home.nix
-              ./nix/dev/neovim.nix
-              ./nix/dev/git.nix
-              ./nix/dev/python.nix
-            ];
-          };
-        }
-      ];
-    };
+      "tower" = nixpkgs.lib.nixosSystem {
+        inherit system pkgs;
+        modules = [
+          ./nixos/tower/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.detroyejr = {
+              imports = [
+                ./nix/home.nix
+                ./nix/dev/neovim.nix
+                ./nix/dev/git.nix
+                ./nix/dev/python.nix
+              ];
+            };
+          }
+        ];
+      };
 
-    nixosConfigurations.brick = nixpkgs.lib.nixosSystem {
-      inherit pkgs;
-      system = "x86_64-linux";
-      modules = [
-        ./nixos/brick/configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.extraSpecialArgs = {inherit nix-colors colorSchemeName colorScheme wallpaper;};
-          home-manager.users.detroyejr = {
-            imports = [
-              ./nix/home.nix
-              ./nix/dev
-            ];
-          };
-        }
-      ];
+      "brick" = nixpkgs.lib.nixosSystem {
+        inherit system pkgs;
+        modules = [
+          ./nixos/brick/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = {inherit nix-colors colorSchemeName colorScheme wallpaper;};
+            home-manager.users.detroyejr = {
+              imports = [
+                ./nix/home.nix
+                ./nix/dev
+              ];
+            };
+          }
+        ];
+      };
     };
   };
 }
