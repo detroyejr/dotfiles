05ef7c960d31bd8c22e719949ab6fc1d9140ceff
detroyejr <detroyejr@outlook.com>
2024-07-07 23:59:10 -0400

    fix(steam): use the right conditional

--- nixos/steam.nix
+++ nixos/steam.nix
@@ -10,12 +10,15 @@
       package =
         pkgs.steam-small.override
         {
-          extraEnv = lib.mkIf isNvidia {
-            __NV_PRIME_RENDER_OFFLOAD = 1;
-            __NV_PRIME_RENDER_OFFLOAD_PROVIDER = "NVIDIA-G0";
-            __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-            __VK_LAYER_NV_optimus = "NVIDIA_only";
-          };
+          extraEnv =
+            if isNvidia
+            then {
+              __NV_PRIME_RENDER_OFFLOAD = 1;
+              __NV_PRIME_RENDER_OFFLOAD_PROVIDER = "NVIDIA-G0";
+              __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+              __VK_LAYER_NV_optimus = "NVIDIA_only";
+            }
+            else {};
         };
     };
     gamemode.enable = true;
