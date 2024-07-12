d769e88e041647dad26a8532db6baa02f185435f
detroyejr <detroyejr@outlook.com>
2024-07-12 17:11:53 -0400

    feat(nvim): which-key v3 update

--- dotfiles/nvim/lua/user/whichkey.lua
+++ dotfiles/nvim/lua/user/whichkey.lua
@@ -1,185 +1,95 @@
-local status_ok, which_key = pcall(require, "which-key")
+local status_ok, wk = pcall(require, "which-key")
 if not status_ok then
-	return
+  return
 end
 
-local setup = {
-	plugins = {
-		marks = true, -- shows a list of your marks on ' and `
-		registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
-		spelling = {
-			enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-			suggestions = 20, -- how many suggestions should be shown in the list?
-		},
-		-- the presets plugin, adds help for a bunch of default keybindings in Neovim
-		-- No actual key bindings are created
-		presets = {
-			operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
-			motions = false, -- adds help for motions
-			text_objects = false, -- help for text objects triggered after entering an operator
-			windows = true, -- default bindings on <c-w>
-			nav = true, -- misc bindings to work with windows
-			z = true, -- bindings for folds, spelling and others prefixed with z
-			g = true, -- bindings for prefixed with g
-		},
-	},
-	icons = {
-		breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-		separator = "➜", -- symbol used between a key and it's label
-		group = "+", -- symbol prepended to a group
-	},
-	popup_mappings = {
-		scroll_down = "<c-d>", -- binding to scroll down inside the popup
-		scroll_up = "<c-u>", -- binding to scroll up inside the popup
-	},
-	window = {
-		border = "rounded", -- none, single, double, shadow
-		position = "bottom", -- bottom, top
-		margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-		padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-		winblend = 0,
-	},
-	layout = {
-		height = { min = 4, max = 25 }, -- min and max height of the columns
-		width = { min = 20, max = 50 }, -- min and max width of the columns
-		spacing = 3, -- spacing between columns
-		align = "left", -- align columns left, center or right
-	},
-	ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
-	hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
-	show_help = true, -- show help message on the command line when the popup is visible
-	triggers = "auto", -- automatically setup triggers
-	triggers_blacklist = {
-		-- list of mode / prefixes that should never be hooked by WhichKey
-		-- this is mostly relevant for key maps that start with a native binding
-		-- most people should not need to change this
-		i = { "j", "k" },
-		v = { "j", "k" },
-	},
-}
+wk.setup()
 
-local opts = {
-	mode = "n", -- NORMAL mode
-	prefix = "<leader>",
-	buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-	silent = true, -- use `silent` when creating keymaps
-	noremap = true, -- use `noremap` when creating keymaps
-	nowait = true, -- use `nowait` when creating keymaps
-}
+-- Top level
+wk.add({
+  { "<leader>e", "<cmd>Explore<cr>",              desc = "Explorer" },
+  { "<leader>f", "<cmd>Telescope find_files<cr>", desc = "Find File", mode = "n" },
+})
 
-local tmux = os.getenv("TMUX")
-if tmux then
-	TERM = {
-		name = "Terminal",
-		f = {
-			"<cmd>!tmux display-popup -h 75\\% -w 75\\% -E "
-				.. '"tmux new-session -A '
-				.. "-s $(tmux display-message "
-				.. "-p '\\#{session_name}')-shell\" "
-				.. "</dev/null >/dev/null 2>&1 & <CR>"
-        .. "<CR>",
-			"Float",
-		},
-		h = { "<cmd>!tmux split-window<CR><CR>", "Horizontal" },
-		v = { "<cmd>!tmux split-window -h<CR><CR>", "Vertical" },
-	}
-else
-	TERM = {
-		name = "Terminal",
-		f = { "<cmd>terminal<cr>", "Float" },
-		h = { "<cmd>new term://${SHELL}<cr>", "Horizontal" },
-		v = { "<cmd>vnew term://${SHELL}<cr>", "Vertical" },
-	}
-end
+-- Telescope
+wk.add({
+  { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File", mode = "n" },
+  { "<leader>F",  "<cmd>Telescope find_files<cr>", desc = "Find Text", mode = "n" },
+})
 
-local mappings = {
-	["b"] = {
-		"<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown{previewer = false})<cr>",
-		"Buffers",
-	},
-	["e"] = { "<cmd>Explore<cr>", "Explorer" },
-	["E"] = { "<cmd>LExplore<cr>", "Explorer (Split)" },
-	["w"] = { "<cmd>w!<CR>", "Save" },
-	["q"] = { "<cmd>q!<CR>", "Quit" },
-	["c"] = { "<cmd>Bdelete!<CR>", "Close Buffer" },
-	["h"] = { "<cmd>nohlsearch<CR>", "No Highlight" },
-	["f"] = {
-		"<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
-		"Find files",
-	},
-	["F"] = { "<cmd>Telescope live_grep<cr>", "Find Text" },
-  T = {
-    F = { "<cmd>Telescope live_grep<cr>", "Find Text" },
-    f = {
-      "<cmd>Telescope find_files<cr>",
-      "Find files",
-    },
-
+-- LSP
+wk.add({
+  { "<leader>l", group = "LSP" },
+  {
+    "<leader>lf",
+    "<cmd>lua require('conform').format({ lsp_fallback = 'always', async = true })<cr>",
+    desc = "Format",
+    mode = "n",
   },
-	g = {
-		name = "Git",
-		j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
-		k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
-		l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
-		p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
-		r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
-		R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
-		s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
-		u = {
-			"<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
-			"Undo Stage Hunk",
-		},
-		o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
-		b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-		-- c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
-		d = {
-			"<cmd>Gitsigns diffthis HEAD<cr>",
-			"Diff",
-		},
-	},
-	l = {
-		name = "LSP",
-		a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
-		D = { "<cmd>Telescope diagnostics<cr>", "Document Diagnostics" },
-		d = { "<cmd>lua vim.lsp.buf.definition()<cr>", "Document Diagnostics" },
-		k = { "<cmd>lua vim.lsp.buf.hover()<cr>", "Document Diagnostics" },
-		j = { "<cmd>lua vim.lsp.buf.signature_help()<cr>", "Document Diagnostics" },
-		f = { "<cmd>lua require('conform').format({ lsp_fallback = 'always', async = true })<cr>", "Format" },
-		i = { "<cmd>LspInfo<cr>", "Info" },
-		I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
-		l = { "<cmd>lua vim.diagnostic.goto_next()<CR>", "Next Diagnostic" },
-		h = { "<cmd>lua vim.diagnostic.goto_prev()<cr>", "Prev Diagnostic" },
-		q = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix" },
-		r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
-		s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
-		S = { "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", "Workspace Symbols" },
-	},
-	s = {
-		name = "Search",
-		b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-		c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
-		h = { "<cmd>Telescope help_tags<cr>", "Find Help" },
-		M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
-		r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
-		R = { "<cmd>Telescope registers<cr>", "Registers" },
-		k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
-		C = { "<cmd>Telescope commands<cr>", "Commands" },
-	},
-	t = TERM,
-}
+  { "<leader>l", "<cmd>lua vim.diagnostic.goto_next()<CR>", "Next Diagnostic" },
+  { "<leader>h", "<cmd>lua vim.diagnostic.goto_prev()<cr>", "Prev Diagnostic" },
+})
 
-local vopts = {
-	mode = "v", -- VISUAL mode
-	prefix = "<leader>",
-	buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-	silent = true, -- use `silent` when creating keymaps
-	noremap = true, -- use `noremap` when creating keymaps
-	nowait = true, -- use `nowait` when creating keymaps
-}
-local vmappings = {
-	["/"] = { '<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>', "Comment" }
-}
+-- Terminal
+if os.getenv("TMUX") then
+  wk.add({
+    { "<leader>t",  group = "Terminal" },
+    {
+      "<leader>tf",
+      "<cmd>!tmux display-popup -h 75\\% -w 75\\% -E "
+      .. '"tmux new-session -A '
+      .. "-s $(tmux display-message "
+      .. "-p '\\#{session_name}')-shell\" "
+      .. "</dev/null >/dev/null 2>&1 & <CR>"
+      .. "<CR>",
+      desc = "Float",
+    },
+    { "<leader>th", "<cmd>!tmux split-window<CR><CR>",    desc = "Horizontal" },
+    { "<leader>tv", "<cmd>!tmux split-window -h<CR><CR>", desc = "Vertical" },
+  })
+else
+  wk.add({
+    { "<leader>tf", "<cmd>terminal<cr>",             desc = "Float" },
+    { "<leader>th", "<cmd>new term://${SHELL}<cr>",  desc = "Horizontal" },
+    { "<leader>tv", "<cmd>vnew term://${SHELL}<cr>", desc = "Vertical" },
+  })
+end
 
-which_key.setup(setup)
-which_key.register(mappings, opts)
-which_key.register(vmappings, vopts)
+-- TODO: ADD git back.
+--   g = {
+--     name = "Git",
+--     j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
+--     k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
+--     l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
+--     p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
+--     r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
+--     R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
+--     s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
+--     u = {
+--       "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
+--       "Undo Stage Hunk",
+--     },
+--     o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
+--     b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+--     -- c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
+--     d = {
+--       "<cmd>Gitsigns diffthis HEAD<cr>",
+--       "Diff",
+--     },
+--   },
+--   l = {
+--     name = "LSP",
+--     a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+--     D = { "<cmd>Telescope diagnostics<cr>", "Document Diagnostics" },
+--     d = { "<cmd>lua vim.lsp.buf.definition()<cr>", "Document Diagnostics" },
+--     k = { "<cmd>lua vim.lsp.buf.hover()<cr>", "Document Diagnostics" },
+--     j = { "<cmd>lua vim.lsp.buf.signature_help()<cr>", "Document Diagnostics" },
+--     f = { "<cmd>lua require('conform').format({ lsp_fallback = 'always', async = true })<cr>", "Format" },
+--     i = { "<cmd>LspInfo<cr>", "Info" },
+--     I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
+--     l = { "<cmd>lua vim.diagnostic.goto_next()<CR>", "Next Diagnostic" },
+--     h = { "<cmd>lua vim.diagnostic.goto_prev()<cr>", "Prev Diagnostic" },
+--     q = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix" },
+--     r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
+--     s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
+--     S = { "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", "Workspace Symbols" },
