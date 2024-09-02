b2cf2087c76402f17618929cfce5c7d776acd34a
detroyejr <detroyejr@outlook.com>
2024-09-02 10:57:11 -0400

    wezterm: theme tweaks

--- nix/window/hyprland/wezterm.nix
+++ nix/window/hyprland/wezterm.nix
@@ -1,13 +1,12 @@
-{ colorScheme, ...}:
-{
+{colorScheme, ...}: {
   programs.wezterm = {
     enable = true;
     enableZshIntegration = true;
     colorSchemes = {
       custom = with colorScheme.colors; {
-        ansi = [ base00 base08 base0B base0A base0D base0E base0C base05 ];
+        ansi = [base00 base08 base0B base0A base0D base0E base0C base05];
         background = base00;
-        brights = [ base03 base08 base0B base0A base0D base0E base0C base07 ];
+        brights = [base03 base08 base0B base0A base0D base0E base0C base07];
         compose_cursor = base06;
         cursor_bg = base05;
         cursor_fg = base00;
@@ -44,34 +43,34 @@
       };
     };
     extraConfig = with colorScheme.colors; ''
-        config = {
-          color_scheme = "custom",
-          cursor_blink_rate = 0,
-          font = wezterm.font("BlexMono Nerd Font Mono"),
-          font_size = 14,
-          use_fancy_tab_bar = false,
-          window_background_opacity = 0.8,
-          window_frame = {
-              active_titlebar_bg = "${base03}",
-              active_titlebar_fg = "${base05}",
-              active_titlebar_border_bottom = "${base03}",
-              border_left_color = "${base01}",
-              border_right_color = "${base01}",
-              border_bottom_color = "${base01}",
-              border_top_color = "${base01}",
-              button_bg = "${base01}",
-              button_fg = "${base05}",
-              button_hover_bg = "${base05}",
-              button_hover_fg = "${base03}",
-              inactive_titlebar_bg = "${base01}",
-              inactive_titlebar_fg = "${base05}",
-              inactive_titlebar_border_bottom = "${base03}",
-          },
-          command_palette_bg_color = "${base01}",
-          command_palette_fg_color = "${base05}",
-          command_palette_font_size = 10,
-        }
-        return config
-      '';
+      config = {
+        color_scheme = "custom",
+        cursor_blink_rate = 0,
+        font = wezterm.font("BlexMono Nerd Font Mono"),
+        font_size = 14,
+        use_fancy_tab_bar = false,
+        window_background_opacity = 0.8,
+        window_frame = {
+            active_titlebar_bg = "${base03}",
+            active_titlebar_fg = "${base05}",
+            active_titlebar_border_bottom = "${base03}",
+            border_left_color = "${base01}",
+            border_right_color = "${base01}",
+            border_bottom_color = "${base01}",
+            border_top_color = "${base01}",
+            button_bg = "${base01}",
+            button_fg = "${base05}",
+            button_hover_bg = "${base05}",
+            button_hover_fg = "${base03}",
+            inactive_titlebar_bg = "${base01}",
+            inactive_titlebar_fg = "${base05}",
+            inactive_titlebar_border_bottom = "${base03}",
+        },
+        command_palette_bg_color = "${base01}",
+        command_palette_fg_color = "${base05}",
+        command_palette_font_size = 10,
+      }
+      return config
+    '';
   };
 }
