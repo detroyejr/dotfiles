4d447d2d9433e2514a239faba35bbd12e2b51741
detroyejr <detroyejr@outlook.com>
2024-09-05 10:03:06 -0400

    plex: bump server version

--- nixos/plex.nix
+++ nixos/plex.nix
@@ -1,14 +1,18 @@
-{pkgs, ...}: let
-  myPlex = pkgs.plex.overrideAttrs (_: rec {
-    version = "1.40.5.8854-f36c552fd";
-    src = pkgs.fetchurl {
-      url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-      hash = "sha256-34DHJQbGiXJndCWOplNE5aics82CcR33lj28AAQvI8Y=";
-    };
-  });
-in {
+{ pkgs, ... }:
+let
+  myPlex = pkgs.plex.override {
+    plexRaw = pkgs.plexRaw.overrideAttrs (out: rec {
+      version = "v.1.40.5.8921-836b34c27";
+      src = pkgs.fetchurl {
+        url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
+        hash = "";
+      };
+    });
+  };
+  in
+{
   fileSystems."/run/media/Media" = {
-    device = "192.168.1.107:/mnt/nas0/Media";
+    device = "191.168.1.107:/mnt/nas0/Media";
     fsType = "nfs";
     options = ["nfsvers=4" "rw"];
   };
