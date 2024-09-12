216bc76f56fdd57262e8ef4cb9d8992817bac57f
detroyejr <detroyejr@outlook.com>
2024-09-12 10:18:53 -0400

    binary-cache: use local cache by default

--- nixos/default.nix
+++ nixos/default.nix
@@ -1,7 +1,19 @@
 {pkgs, isNvidia, ...}: {
   i18n.defaultLocale = "en_US.UTF-8";
   networking.networkmanager.enable = true;
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix = {
+    settings = {
+      experimental-features = ["nix-command" "flakes"];
+      substituters = [
+        "http://mini-1.lan"
+      ];
+      trusted-public-keys = [ "mini-1.lan:Qaw4+6mWCHqNCNL7Vnbo7KXFjjbyl64RaAMdCSEGzKI=" ];
+      trusted-users = [
+        "root"
+        "detroyejr"
+      ];
+    };
+  };
   programs.zsh.enable = true;
   services.ntp.enable = true;
   time.timeZone = "America/New_York";
