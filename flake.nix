5b0a5b751dc30fffc75ef68f72203e15a9e3b286
detroyejr <detroyejr@outlook.com>
2024-09-12 15:37:10 -0400

    flake: document nixpkgs patching

--- flake.nix
+++ flake.nix
@@ -16,153 +16,163 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    nixos-hardware,
-    hyprland,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-    system = "x86_64-linux";
-    base = nixpkgs.legacyPackages.${system};
-    patched = (import nixpkgs { inherit system; }).applyPatches {
-      name = "patched";
-      src = nixpkgs;
-
-      patches = [];
-    };
-    pkgs = import patched {
-      inherit system;
-      config = {
-        allowUnfree = true;
-        allowBroken = true;
-        cudaSupport = false;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      nixos-hardware,
+      hyprland,
+      ...
+    }@inputs:
+    let
+      inherit (self) outputs;
+      system = "x86_64-linux";
+      patched = (import ./patched.nix) { inherit nixpkgs system; };
+      pkgs = import patched {
+        inherit system;
+        config = {
+          allowUnfree = true;
+          allowBroken = true;
+          cudaSupport = false;
+        };
+        overlays = [ (import ./overlays.nix) ];
       };
-      overlays = [
-        (import ./overlays.nix)
-      ];
-    };
 
-    nix-colors = import ./colors.nix;
-    colorSchemeName = "rose-pine";
-    colorScheme = nix-colors.${colorSchemeName}.colorScheme;
-    wallpaper = nix-colors.${colorSchemeName}.wallpaper;
-    rofi-background = nix-colors.${colorSchemeName}.rofi;
+      nix-colors = import ./colors.nix;
+      colorSchemeName = "rose-pine";
+      colorScheme = nix-colors.${colorSchemeName}.colorScheme;
+      wallpaper = nix-colors.${colorSchemeName}.wallpaper;
+      rofi-background = nix-colors.${colorSchemeName}.rofi;
 
-    default-home-configuration = {
-      home-manager.useGlobalPkgs = true;
-      home-manager.useUserPackages = true;
-      home-manager.extraSpecialArgs = {
-        inherit nix-colors colorSchemeName colorScheme wallpaper hyprland rofi-background;
-        isNvidia = false;
+      default-home-configuration = {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.extraSpecialArgs = {
+          inherit
+            nix-colors
+            colorSchemeName
+            colorScheme
+            wallpaper
+            hyprland
+            rofi-background
+            ;
+          isNvidia = false;
+        };
+        home-manager.users.detroyejr = {
+          imports = [
+            ./nix/home.nix
+            ./nix/dev
+            ./nix/window/hyprland
+          ];
+        };
       };
-      home-manager.users.detroyejr = {
-        imports = [
+    in
+    {
+      formatter.${system} = nixpkgs.legacyPackages.${system}.alejandra;
+
+      # A default configuration that should work on non-NixOS machines.
+      homeConfigurations.detroyejr = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        extraSpecialArgs = {
+          inherit colorSchemeName colorScheme;
+        };
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [
           ./nix/home.nix
           ./nix/dev
-          ./nix/window/hyprland
         ];
+      };
+
+      nixosConfigurations = {
+        "xps" = nixpkgs.lib.nixosSystem {
+          inherit system pkgs;
+          specialArgs = {
+            inherit inputs outputs system;
+            isNvidia = true;
+            isFprint = true;
+          };
+          modules = [
+            ./nixos/xps/configuration.nix
+            nixos-hardware.nixosModules.dell-xps-15-9520-nvidia
+            home-manager.nixosModules.home-manager
+            default-home-configuration
+          ];
+        };
+        "mini" = nixpkgs.lib.nixosSystem {
+          inherit system pkgs;
+          specialArgs = {
+            inherit inputs outputs system;
+            isNvidia = false;
+            isFprint = false;
+          };
+          modules = [
+            ./nixos/mini/configuration.nix
+            # Running the 3070, but I think this is close enough.
+            nixos-hardware.nixosModules.dell-optiplex-3050
+            home-manager.nixosModules.home-manager
+            default-home-configuration
+          ];
+        };
+        "tower" = nixpkgs.lib.nixosSystem {
+          inherit system pkgs;
+          specialArgs = {
+            inherit inputs outputs system;
+            isNvidia = false;
+            isFprint = false;
+          };
+          modules = [
+            ./nixos/tower/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.detroyejr = {
+                imports = [
+                  ./nix/home.nix
+                  ./nix/dev/neovim.nix
+                  ./nix/dev/git.nix
+                  ./nix/dev/python.nix
+                ];
+              };
+            }
+          ];
+        };
+        "brick" = nixpkgs.lib.nixosSystem {
+          inherit system pkgs;
+          specialArgs = {
+            inherit inputs outputs system;
+            isNvidia = true;
+            isFprint = false;
+          };
+          modules = [
+            ./nixos/brick/configuration.nix
+            home-manager.nixosModules.home-manager
+            nixos-hardware.nixosModules.dell-xps-15-9570-nvidia
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = {
+                inherit
+                  nix-colors
+                  colorSchemeName
+                  colorScheme
+                  wallpaper
+                  rofi-background
+                  ;
+                isNvidia = true;
+              };
+              home-manager.users.detroyejr = {
+                imports = [
+                  ./nix/home.nix
+                  ./nix/dev
+                ];
+              };
+            }
+          ];
+        };
       };
     };
-  in {
-    formatter.${system} = nixpkgs.legacyPackages.${system}.alejandra;
-
-    # A default configuration that should work on non-NixOS machines.
-    homeConfigurations.detroyejr = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-
-      extraSpecialArgs = {inherit colorSchemeName colorScheme;};
-      # Specify your home configuration modules here, for example,
-      # the path to your home.nix.
-      modules = [
-        ./nix/home.nix
-        ./nix/dev
-      ];
-    };
-
-    nixosConfigurations = {
-      "xps" = nixpkgs.lib.nixosSystem {
-        inherit system pkgs;
-        specialArgs = {
-          inherit inputs outputs system;
-          isNvidia = true;
-          isFprint = true;
-        };
-        modules = [
-          ./nixos/xps/configuration.nix
-          nixos-hardware.nixosModules.dell-xps-15-9520-nvidia
-          home-manager.nixosModules.home-manager
-          default-home-configuration
-        ];
-      };
-      "mini" = nixpkgs.lib.nixosSystem {
-        inherit system pkgs;
-        specialArgs = {
-          inherit inputs outputs system;
-          isNvidia = false;
-          isFprint = false;
-        };
-        modules = [
-          ./nixos/mini/configuration.nix
-          # Running the 3070, but I think this is close enough.
-          nixos-hardware.nixosModules.dell-optiplex-3050
-          home-manager.nixosModules.home-manager
-          default-home-configuration
-        ];
-      };
-      "tower" = nixpkgs.lib.nixosSystem {
-        inherit system pkgs;
-        specialArgs = {
-          inherit inputs outputs system;
-          isNvidia = false;
-          isFprint = false;
-        };
-        modules = [
-          ./nixos/tower/configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.detroyejr = {
-              imports = [
-                ./nix/home.nix
-                ./nix/dev/neovim.nix
-                ./nix/dev/git.nix
-                ./nix/dev/python.nix
-              ];
-            };
-          }
-        ];
-      };
-      "brick" = nixpkgs.lib.nixosSystem {
-        inherit system pkgs;
-        specialArgs = {
-          inherit inputs outputs system;
-          isNvidia = true;
-          isFprint = false;
-        };
-        modules = [
-          ./nixos/brick/configuration.nix
-          home-manager.nixosModules.home-manager
-          nixos-hardware.nixosModules.dell-xps-15-9570-nvidia
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = {
-              inherit nix-colors colorSchemeName colorScheme wallpaper rofi-background;
-              isNvidia = true;
-            };
-            home-manager.users.detroyejr = {
-              imports = [
-                ./nix/home.nix
-                ./nix/dev
-              ];
-            };
-          }
-        ];
-      };
-    };
-  };
 }
