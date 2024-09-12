2dba5627770a73298e7e399c3820c7f9b3072b58
detroyejr <detroyejr@outlook.com>
2024-09-12 15:06:01 -0400

    flake: bump

--- flake.nix
+++ flake.nix
@@ -31,12 +31,7 @@
       name = "patched";
       src = nixpkgs;
 
-      patches = [
-        (base.fetchpatch {
-          url = "https://github.com/NixOS/nixpkgs/pull/341226.patch";
-          hash = "sha256-asnviq/pGFUg1iSbcH+IqfaocODRnMAcTfPyU48CImo=";
-        })
-      ];
+      patches = [];
     };
     pkgs = import patched {
       inherit system;
