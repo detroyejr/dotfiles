d4727206550bd0b33df308c1fc018ff4e5a2cc2e
detroyejr <detroyejr@outlook.com>
2024-09-17 11:13:25 -0400

    nix: basic builder script

---
+++ mini-builder.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash --packages bash gh git
+
+eval $(ssh-agent)
+ssh-add ~/.ssh/github_rsa
+
+git checkout origin/update_flake_lock_action --detach
+
+if [[ $(git log --oneline | grep $(git log --pretty --oneline -n 1 | awk '{ print $1 }') | wc -l) -gt 0 ]]; then
+  for system in xps mini potato; do
+    nixos-rebuild build --flake $HOME/.config/dotfiles#${system}
+    exit_code=$?
+    [[ $exit_code == 1 ]] && exit 1
+  done
+fi;
+
+# Merge
+git checkout main && \
+  git stash && \
+  git merge origin/update_flake_lock_action && \
+  git stash apply
+rm -r result
+
