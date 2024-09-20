7ef9a03c3c76f4acb2cbdf31b52def3e1946a345
detroyejr <detroyejr@outlook.com>
2024-09-20 17:53:36 -0400

    waybar: initial setup

--- nix/window/hyprland/waybar.nix
+++ nix/window/hyprland/waybar.nix
@@ -1,6 +1,13 @@
-{ ... }:
+{ colorScheme, ... }:
 {
   programs.waybar = {
     enable = true;
   };
+
+  home.file.".config/waybar/config".source = ../../../dotfiles/waybar/config;
+  home.file.".config/waybar/style.css".source = ../../../dotfiles/waybar/style.css;
+  home.file.".config/waybar/config.css".text = ''
+    @define-color accent #${colorScheme.colors.base08};
+  '';
+
 }
