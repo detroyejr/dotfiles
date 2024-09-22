25f79706aa2d691e495b5b711c681a3f525af12b
detroyejr <detroyejr@outlook.com>
2024-09-22 00:01:55 -0400

    potato: revert tlp

--- nixos/potato/configuration.nix
+++ nixos/potato/configuration.nix
@@ -16,13 +16,6 @@
   networking.hostName = "potato"; # Define your hostname.
 
   programs.firefox.enable = true;
-  services.tlp = {
-    enable = true;
-    settings = {
-      INTEL_GPU_MIN_FREQ_ON_AC = 500;
-      INTEL_GPU_MIN_FREQ_ON_BAT = 500;
-    };
-  };
   environment.systemPackages = [ pkgs.plex-desktop ];
 
   system.stateVersion = "24.05"; # Did you read the comment?
