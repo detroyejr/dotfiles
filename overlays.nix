f4ee12ebc273f30168b475b7f02162781b43d976
detroyejr <detroyejr@outlook.com>
2024-10-17 13:48:17 -0400

    overlay: add hyperion-obs patch till this is merged

--- overlays.nix
+++ overlays.nix
@@ -1,28 +1,42 @@
-final: prev: {
-  # fprintd = prev.fprintd.overrideAttrs {
-  #   mesonCheckFlags = [
-  #     "--no-suite"
-  #     "fprintd:TestPamFprintd"
-  #   ];
-  # };
+# NOTE: Example patches.
+# fprintd = prev.fprintd.overrideAttrs {
+#   mesonCheckFlags = [
+#     "--no-suite"
+#     "fprintd:TestPamFprintd"
+#   ];
+# };
 
-  # nix = prev.nix.overrideAttrs {
-  #   # NOTE: Patch valid for nix v2.18.5
-  #   patches = [
-  #     (builtins.toFile "nice.patch" ''
-  #       diff --git a/src/libstore/build/local-derivation-goal.cc b/src/libstore/build/local-derivation-goal.cc
-  #       index 5ab231f24..de68d6768 100644
-  #       --- a/src/libstore/build/local-derivation-goal.cc
-  #       +++ b/src/libstore/build/local-derivation-goal.cc
-  #       @@ -1740,6 +1740,7 @@ void LocalDerivationGoal::runChild()
-  #                } catch (SysError &) { }
-  #
-  #        #if __linux__
-  #       +        nice(15);
-  #                if (useChroot) {
-  #
-  #                    userNamespaceSync.writeSide = -1;
-  #     '')
-  #   ];
-  # };
+# nix = prev.nix.overrideAttrs {
+#   # NOTE: Patch valid for nix v2.18.5
+#   patches = [
+#     (builtins.toFile "nice.patch" ''
+#       diff --git a/src/libstore/build/local-derivation-goal.cc b/src/libstore/build/local-derivation-goal.cc
+#       index 5ab231f24..de68d6768 100644
+#       --- a/src/libstore/build/local-derivation-goal.cc
+#       +++ b/src/libstore/build/local-derivation-goal.cc
+#       @@ -1740,6 +1740,7 @@ void LocalDerivationGoal::runChild()
+#                } catch (SysError &) { }
+#
+#        #if __linux__
+#       +        nice(15);
+#                if (useChroot) {
+#
+#                    userNamespaceSync.writeSide = -1;
+#     '')
+#   ];
+# };
+
+final: prev: {
+  # NOTE: obs needs to be overrided in a different way.
+  obs-studio-plugins = prev.obs-studio-plugins // {
+    obs-hyperion = prev.obs-studio-plugins.obs-hyperion.overrideAttrs {
+      patches = [
+        (final.fetchpatch {
+          url = "https://raw.githubusercontent.com/NixOS/nixpkgs/c3ceedeac1ac9cbf8f0f99d96ff87b56e5dd3df8/pkgs/applications/video/obs-studio/plugins/obs-hyperion/check-state-changed.patch";
+          hash = "sha256-cQTq8H9aUTpdUdaK94qIjTxqS9n4mVWlfZlxUoL4+JU=";
+        })
+      ];
+    };
+  };
 }
+    
