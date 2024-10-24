a302652b343c56546645f99cd48c682f53ca4c15
Jon De Troye <detroyejr@outlook.com>
2024-10-23 22:41:26 -0400

    neovim: minidiff toggle overlay keymap

--- dotfiles/nvim/lua/plugins/mini.lua
+++ dotfiles/nvim/lua/plugins/mini.lua
@@ -1,12 +1,15 @@
 return {
-	'echasnovski/mini.nvim',
-	version = false,
-	config = function()
-		require('mini.bufremove').setup()
-		require('mini.comment').setup()
-		require('mini.diff').setup()
-		require('mini.git').setup()
-		require('mini.icons').setup()
-		require('mini.statusline').setup()
-	end,
+  'echasnovski/mini.nvim',
+  version = false,
+  config = function()
+    require('mini.bufremove').setup()
+    require('mini.comment').setup()
+    require('mini.diff').setup()
+    require('mini.git').setup()
+    require('mini.icons').setup()
+    require('mini.statusline').setup()
+  end,
+  keys = {
+    { "dt", "<cmd>lua MiniDiff.toggle_overlay()<CR>", silent = true },
+  }
 }
