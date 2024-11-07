59af4d168e7d61baeab87c0851408672e4c1538b
detroyejr <detroyejr@outlook.com>
2024-11-07 00:21:23 -0500

    WIP: use .libPaths to make ark know about available libraries in nix

--- dotfiles/nvim/lua/config/ark.lua
+++ dotfiles/nvim/lua/config/ark.lua
@@ -5,31 +5,30 @@ M = {}
 local lsp = require 'lspconfig'
 local configs = require 'lspconfig.configs'
 
-if not configs.ark then
-  configs.ark = {
-    default_config = {
-      cmd = vim.lsp.rpc.connect('127.0.0.1', M.on_attach()),
-      filetypes = { 'r', 'R' },
-      single_file_support = true,
-      root_dir = function(fname)
-        return lsp.util.root_pattern("DESCRIPTION", "NAMESPACE", ".Rbuildignore", ".RProj", ".Rproj", ".rproj")(
-              fname) or
-            vim.loop.os_homedir()
-      end,
-    },
-  }
+M.on_attach = function()
+  local f = assert(io.popen('ark-lsp.py', 'r'))
+  local lsp_port = assert(f:read('*a')); f:close()
+  return tonumber(lsp_port)
 end
 
 M.setup = function()
+  if not configs.ark then
+    configs.ark = {
+      default_config = {
+        cmd = vim.lsp.rpc.connect('127.0.0.1', M.on_attach()),
+        filetypes = { 'r', 'R' },
+        single_file_support = true,
+        root_dir = function(fname)
+          return lsp.util.root_pattern("DESCRIPTION", "NAMESPACE", ".Rbuildignore", ".RProj", ".Rproj", ".rproj")(
+                fname) or
+              vim.loop.os_homedir()
+        end,
+      },
+    }
+  end
   lsp.ark.setup {
     on_attach = M.on_attach
   }
-end
-
-M.on_attach = function()
-  local f = assert(io.popen('ark-lsp', 'r'))
-  local lsp_port = assert(f:read('*a')); f:close()
-  return tonumber(lsp_port)
 end
 
 M.setup()
