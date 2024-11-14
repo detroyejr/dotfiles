34528d4444a19f5561f69b6e26038e4410eaafa4
detroyejr <detroyejr@outlook.com>
2024-11-14 11:32:25 -0500

    obs-hyperion: my PR to fix this was mergedc

--- overlays.nix
+++ overlays.nix
@@ -27,17 +27,6 @@
 # };
 
 final: prev: {
-  # NOTE: obs needs to be overrided in a different way.
-  obs-studio-plugins = prev.obs-studio-plugins // {
-    obs-hyperion = prev.obs-studio-plugins.obs-hyperion.overrideAttrs {
-      patches = [
-        (final.fetchpatch {
-          url = "https://raw.githubusercontent.com/NixOS/nixpkgs/c3ceedeac1ac9cbf8f0f99d96ff87b56e5dd3df8/pkgs/applications/video/obs-studio/plugins/obs-hyperion/check-state-changed.patch";
-          hash = "sha256-cQTq8H9aUTpdUdaK94qIjTxqS9n4mVWlfZlxUoL4+JU=";
-        })
-      ];
-    };
-  };
   # Disable while coredump during test.
   calibre = prev.calibre.overrideAttrs {
     doInstallCheck = false;
