ead54a1b7552cef58b40e3ef2cfa69f808edae33
detroyejr <detroyejr@outlook.com>
2024-12-07 00:24:33 -0500

    fix: add holiday splash objects

--- dotfiles/hyprland/Splashes.hpp
+++ dotfiles/hyprland/Splashes.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <string>
+#include <format>
+#include <chrono>
 #include <vector>
 
 namespace NSplashes {
@@ -108,4 +110,37 @@ namespace NSplashes {
       "John 1:52 - And he said to him, “Truly, truly, I say to you, you will see "
       "heaven opened, and the angels of God ascending and descending on the Son "
       "of Man.”"};
+
+
+    inline const std::vector<std::string> SPLASHES_CHRISTMAS = {
+        // clang-format off
+        "Merry Christmas!",
+        "Merry Xmas!",
+        "Ho ho ho",
+        "Santa was here",
+        "Make sure to spend some jolly time with those near and dear to you!",
+        "Have you checked for christmas presents yet?",
+        // clang-format on
+    };
+
+    // ONLY valid near new years.
+    inline static int newYear = []() -> int {
+        auto tt    = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+        auto local = *localtime(&tt);
+
+        if (local.tm_mon < 8 /* decided with a fair die I promise. */)
+            return local.tm_year + 1900;
+        return local.tm_year + 1901;
+    }();
+
+    inline const std::vector<std::string> SPLASHES_NEWYEAR = {
+        // clang-format off
+        "Happy new Year!",
+        "[New year] will be the year of the Linux desktop!",
+        "[New year] will be the year of the Hyprland desktop!",
+        std::format("{} will be the year of the Linux desktop!", newYear),
+        std::format("{} will be the year of the Hyprland desktop!", newYear),
+        std::format("Let's make {} even better than {}!", newYear, newYear - 1),
+        // clang-format on
+    };
 };
