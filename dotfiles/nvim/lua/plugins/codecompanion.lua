e71c5ea034af1431749addaeff3c4cbfac3aa31d
detroyejr <detroyejr@outlook.com>
2024-12-10 15:56:58 -0500

    neovim: add codecompanion plugin

---
+++ dotfiles/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,40 @@
+return {
+  'olimorris/codecompanion.nvim',
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    return require("codecompanion").setup({
+      adapters = {
+        ollama = function()
+          return require("codecompanion.adapters").extend("ollama", {
+            env = {
+              url = "mini-1.lan:11434",
+            },
+            parameters = {
+              sync = true,
+            },
+          })
+        end,
+      },
+      display = {
+        diff = {
+          provider = "mini_diff",
+        },
+      },
+      strategies = {
+        chat = {
+          adapter = "ollama",
+        },
+        inline = {
+          adapter = "ollama",
+        },
+      },
+    })
+  end,
+  keys = {
+    { "<leader>ac", "<cmd>CodeCompanionChat Toggle<cr>", silent = true },
+    { "<leader>ap", "<cmd>CodeCompanion<cr>",            silent = true },
+  },
+}
