33e211cad3f3864876942bd20da44c76f6a66667
detroyejr <detroyejr@outlook.com>
2024-12-20 13:08:18 -0500

    cataclysm-dda: remove tileset and update cc-sounds

--- nixos/cataclysm-dda.nix
+++ nixos/cataclysm-dda.nix
@@ -28,30 +28,23 @@ let
 
   customMods =
     self: super:
-    lib.recursiveUpdate super {
-      soundpack.CC-Sounds = cddaLib.buildSoundPack {
-        modName = "CC-Sounds";
-        version = "2024-01-17";
-        src = pkgs.fetchzip {
-          url = "https://github.com/Fris0uman/CDDA-Soundpacks/releases/download/2024-01-17/CC-Sounds.zip";
-          hash = "sha256-i2b++XPcV1IJeDZF1TC9C325XTn55TVa57xTdtYNV8g=";
+    let 
+      version = "2024-10-27";
+    in
+      lib.recursiveUpdate super {
+        soundpack.CC-Sounds = cddaLib.buildSoundPack {
+          inherit version;
+          modName = "CC-Sounds";
+          src = pkgs.fetchzip {
+            url = "https://github.com/Fris0uman/CDDA-Soundpacks/releases/download/${version}/CC-Sounds.zip";
+            hash = "";
+          };
         };
       };
-      tileset.UndeadPeopleCustom = cddaLib.buildTileSet {
-        modName = "UndeadPeopleCustom";
-        version = "Tutti Frutti (August Release 2) Latest";
-        src = pkgs.fetchzip {
-          url = "https://github.com/Theawesomeboophis/UndeadPeopleTileset/releases/download/8%2F16%2F24/Sprinkles.zip";
-          hash = "sha256-4PVliOuhow9KFd79bDqLdEpoKmbLK+5U4NzNnib+uRU=";
-          stripRoot = false;
-        };
-      };
-    };
 
   cdda = (cddaLib.attachPkgs cddaLib.pkgs cdda-no-mod).withMods (
     mods: with mods.extend customMods; [
       soundpack.CC-Sounds
-      tileset.UndeadPeopleCustom
     ]
   );
 
