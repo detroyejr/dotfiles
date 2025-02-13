f8ccc43ac023b7faad0a972d89146b0fbf574d46
detroyejr <detroyejr@outlook.com>
2025-02-12 20:11:36 -0500

    positron: bump

--- nixos/fonts.nix
+++ nixos/fonts.nix
@@ -1,25 +1,39 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
+let
+
+  input-fonts-nf = pkgs.input-fonts.overrideAttrs (oldAttrs: {
+    patchPhase = ''
+      mkdir tmp
+      for path in InputMono InputSans; do
+        find Input_Fonts/$path -iname "*.ttf" -exec ${pkgs.nerd-font-patcher}/bin/nerd-font-patcher {} -out tmp \;
+        mv tmp/* Input_Fonts/$path 
+      done
+    '';
+  });
+  in
 {
+  config.
   fonts = {
     enableDefaultPackages = true;
     packages = with pkgs; [
-      ubuntu_font_family
+      input-fonts-nf
       nerd-fonts.blex-mono
       nerd-fonts.ubuntu
+      ubuntu_font_family
     ];
 
     fontconfig = {
       defaultFonts = {
         serif = [
-          "BlexMono NF Serif"
+          "Input Serif"
           "Ubuntu"
         ];
         sansSerif = [
-          "BlexMono NF"
+          "Input Serif"
           "Ubuntu"
         ];
         monospace = [
-          "BlexMono NF Mono"
+          "Input Mono"
           "Ubuntu"
         ];
       };
