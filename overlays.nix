2de87f086f54ecb4177146afe2eec16b18a626b2
detroyejr <detroyejr@outlook.com>
2025-03-05 10:09:59 -0500

    positron: no need to patch

--- overlays.nix
+++ overlays.nix
@@ -65,41 +65,6 @@ final: prev: {
       ];
     };
 
-  positron-bin = prev.positron-bin.overrideAttrs (attrs: {
-    src = prev.fetchurl {
-      url = "https://github.com/posit-dev/positron/releases/download/2025.02.0-171/Positron-2025.02.0-171-x64.deb";
-      hash = "sha256-TjQc/Y4Sa2MlLslbygYVFbIk3raArMvYstSiSEYzfo0=";
-    };
-    nativeBuildInputs = attrs.nativeBuildInputs ++ [ prev.wrapGAppsHook ];
-    installPhase = ''
-      runHook preInstall
-      mkdir -p "$out/share"
-      cp -r usr/share/pixmaps "$out/share/pixmaps"
-      cp -r usr/share/positron "$out/share/positron"
-
-      mkdir -p "$out/share/applications"
-      install -m 444 -D usr/share/applications/positron.desktop "$out/share/applications/positron.desktop"
-      substituteInPlace "$out/share/applications/positron.desktop" \
-        --replace-fail \
-        "Icon=co.posit.positron" \
-        "Icon=$out/share/pixmaps/co.posit.positron.png" \
-        --replace-fail \
-        "Exec=/usr/share/positron/positron %F" \
-        "Exec=$out/share/positron/.positron-wrapped %F" \
-        --replace-fail \
-        "/usr/share/positron/positron --new-window %F" \
-        "$out/share/positron/.positron-wrapped --new-window %F"
-
-      # Fix libGL.so not found errors.
-      wrapProgram "$out/share/positron/positron" \
-        --prefix LD_LIBRARY_PATH : "${prev.lib.makeLibraryPath [ prev.libglvnd ]}"
-
-      mkdir -p "$out/bin"
-      ln -s "$out/share/positron/positron" "$out/bin/positron"
-      runHook postInstall
-    '';
-  });
-
   obs-studio-plugins = prev.obs-studio-plugins // {
     obs-hyperion = prev.obs-studio-plugins.obs-hyperion.overrideAttrs {
       patches = [
