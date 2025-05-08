9a7912d2e99509d563167cd55d6c2bbf16d7c2f4
detroyejr <detroyejr@outlook.com>
2025-05-08 18:52:54 -0400

    neovim: replace nvim-cmp with blink and nvim-lspconfig with built-in

---
+++ dotfiles/nvim/lsp/ark.lua
@@ -0,0 +1,13 @@
+return {
+  cmd = { "ark-lsp.py" },
+  filetypes = { 'r', 'R' },
+  root_markers = {
+    '.rproj',
+    '.Rproj',
+    '.RPROJ',
+    '.RProj',
+    'DESCRIPTION',
+    'NAMESPACE',
+    '.git'
+  },
+}
