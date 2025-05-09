9d9ce536b6a714f4f4d34cb5c0650f86773f16d7
detroyejr <detroyejr@outlook.com>
2025-05-09 18:26:26 -0400

    neovim: add MiniGit shortcut

--- dotfiles/nvim/lua/plugins/mini.lua
+++ dotfiles/nvim/lua/plugins/mini.lua
@@ -11,6 +11,10 @@ return {
     require('mini.statusline').setup()
   end,
   keys = {
+    -- Diff
     { "<leader>dt", "<cmd>lua MiniDiff.toggle_overlay()<CR>", silent = true },
+
+    -- Git
+    { "<leader>gs", "<cmd>lua MiniGit.show_at_cursor()<CR>", silent = true },
   }
 }
