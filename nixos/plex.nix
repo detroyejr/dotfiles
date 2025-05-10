798b65ae4d36a62740f45d5697e91d4271283dc5
detroyejr <detroyejr@outlook.com>
2025-05-10 12:42:07 -0400

    plex: don't pin server version

--- nixos/plex.nix
+++ nixos/plex.nix
@@ -1,15 +1,4 @@
 { pkgs, ... }:
-let
-  myPlex = pkgs.plex.override {
-    plexRaw = pkgs.plexRaw.overrideAttrs (out: rec {
-      version = "1.40.5.8921-836b34c27";
-      src = pkgs.fetchurl {
-        url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-        hash = "sha256-B/c9/wAQTkJ+Uzz7oLKzczAYx7U8DFB8DbvT/kwTKjo=";
-      };
-    });
-  };
-in
 {
   fileSystems."/run/media/Media" = {
     device = "192.168.1.107:/mnt/nas0/Media";
@@ -17,7 +6,6 @@ in
     options = ["rw" "relatime" "vers=4.2" "rsize=524288" "wsize=524288" ];
   };
   services.plex = {
-    package = myPlex;
     enable = true;
     openFirewall = true;
     accelerationDevices = [ "*" ];
