d1c76f44066148d622113a23ad3f895516bae009
detroyejr <detroyejr@outlook.com>
2025-05-11 16:43:54 -0400

    mini: update networking

--- nixos/mini/configuration.nix
+++ nixos/mini/configuration.nix
@@ -42,13 +42,19 @@
     ];
   };
 
-  networking.wireless.enable = false;
-  networking.interfaces.enp1s0.ipv4.addresses = [
-    {
-      address = "192.168.1.222";
-      prefixLength = 24;
-    }
-  ];
+  networking = {
+    networkmanager.enable = false;
+    wireless.enable = false;
+    interfaces.enp1s0 = {
+      ipv4.addresses = [
+        {
+          address = "192.168.1.222";
+          prefixLength = 24;
+        }
+      ];
+      useDHCP = true;
+    };
+  };
 
   networking.firewall.allowedTCPPorts = [
     22
