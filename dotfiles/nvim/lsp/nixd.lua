0a8f4d5e06b4e1a2d5098c8fda81790ef102a188
detroyejr <detroyejr@outlook.com>
2025-05-11 16:45:47 -0400

    neovim: nil lsp

--- dotfiles/nvim/lsp/nixd.lua
+++ dotfiles/nvim/lsp/nixd.lua
@@ -1,23 +1,5 @@
 return {
-  cmd = { 'nixd' },
+  cmd = { 'nil' },
   filetypes = { 'nix' },
-  root_markers = { 'flake.nix', 'git' },
-  settings = {
-    nixd = {
-      nixpkgs = {
-        expr = "import <nixpkgs> { }",
-      },
-      formatting = {
-        command = { "nixfmt" },
-      },
-      options = {
-        nixos = {
-          expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.k-on.options',
-        },
-        home_manager = {
-          expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."ruixi@k-on".options',
-        },
-      },
-    },
-  }
+  root_markers = { 'flake.nix', '.git' },
 }
