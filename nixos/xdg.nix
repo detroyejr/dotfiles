49fde77ecb149453cad6ecbadc53f1c7d8b6bf34
detroyejr <detroyejr@outlook.com>
2025-07-03 17:17:43 -0400

    xdg: edit user-dirs

--- nixos/xdg.nix
+++ nixos/xdg.nix
@@ -6,5 +6,19 @@
     XDG_CURSOR_THEME = config.cursor.name;
     XDG_SESSION_DESKTOP = "Hyprland";
     XDG_SESSION_TYPE = "wayland";
+
+  };
+
+  environment.etc = {
+    "xdg/user-dirs.dirs".text = ''
+      XDG_DESKTOP_DIR="$HOME/Desktop"
+      XDG_DOCUMENTS_DIR="$HOME/Documents"
+      XDG_DOWNLOAD_DIR="$HOME/Downloads"
+      XDG_MUSIC_DIR="$HOME/Music"
+      XDG_PICTURES_DIR="$HOME/Documents/Pictures"
+      XDG_PUBLICSHARE_DIR="$HOME/Public"
+      XDG_TEMPLATES_DIR="$HOME/Templates"
+      XDG_VIDEOS_DIR="$HOME/Documents/Videos"
+    '';
   };
 }
