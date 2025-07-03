37a9aba525907aff962ffa4ac2fc6ad9a1b61a63
detroyejr <detroyejr@outlook.com>
2025-07-03 13:36:33 -0400

    gnome-keyring: try some settings to get this working again

--- nixos/devices.nix
+++ nixos/devices.nix
@@ -51,23 +51,36 @@
   # Recommended with pipewire.
   security.rtkit.enable = true;
 
+  services.dbus.packages = [
+    pkgs.gnome-keyring
+    pkgs.gcr
+  ];
+
+  services.xserver.displayManager.sessionCommands = ''
+    eval $(gnome-keyring-daemon --start --daemonize --components=ssh,secrets)
+    export SSH_AUTH_SOCK
+  '';
+
   security = {
     polkit.enable = true;
-    pam.services.hyprlock.text = lib.mkIf config.services.fprintd.enable ''
-      # Account management.
-      account required pam_unix.so
+    pam.services = {
+      login.enableGnomeKeyring = true;
+      hyprlock.text = lib.mkIf config.services.fprintd.enable ''
+        # Account management.
+        account required pam_unix.so
 
-      # Authentication management.
-      auth sufficient pam_unix.so   likeauth try_first_pass nullok
-      auth sufficient ${pkgs.fprintd}/lib/security/pam_fprintd.so # fprintd (order 11400)
-      auth required pam_deny.so
+        # Authentication management.
+        auth sufficient pam_unix.so   likeauth try_first_pass nullok
+        auth sufficient ${pkgs.fprintd}/lib/security/pam_fprintd.so # fprintd (order 11400)
+        auth required pam_deny.so
 
-      # Password management.
-      password sufficient pam_unix.so nullok sha512
+        # Password management.
+        password sufficient pam_unix.so nullok sha512
 
-      # Session management.
-      session required pam_env.so conffile=/etc/pam/environment readenv=0
-      session required pam_unix.so
-    '';
+        # Session management.
+        session required pam_env.so conffile=/etc/pam/environment readenv=0
+        session required pam_unix.so
+      '';
+    };
   };
 }
