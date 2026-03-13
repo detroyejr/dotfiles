86a8832077a1139cafeede9bf03e0afd2dbe5352
detroyejr <detroyejr@outlook.com>
2026-03-13 07:59:19 -0400

    firefox: improve fresh install experience

--- modules/apps/firefox.nix
+++ modules/apps/firefox.nix
@@ -16,12 +16,40 @@ in
 {
   config = lib.mkIf cfg.enable {
     programs.firefox = {
+      autoConfig = ''
+        pref("general.config.filename", "firefox.cfg");
+        pref("general.config.obscure_value", 0);
+        pref("gwfox.plus", 1);
+      '';
+      policies = {
+        GenerativeAI = {
+          Enabled = false;
+        };
+        FirefoxSuggest = {
+          WebSuggestions = false;
+          SponsoredSuggestions = false;
+          ImproveSuggest = false;
+          Locked = false;
+        };
+        FirefoxHome = {
+          Search = false;
+          TopSites = false;
+          SponsoredTopSites = false;
+          Highlights = false;
+          Pocket = false;
+          Stories = false;
+          SponsoredPocket = false;
+          SponsoredStories = false;
+          Snippets = false;
+          Locked = false;
+        };
+      };
       preferences = {
+        "browser.ai.control.default" = "blocked";
         "browser.ml.enable" = false;
+        "browser.startup.homepage" = "http://odp-1:5678";
         "browser.tabs.allow_transparent_browser" = true;
         "browser.tabs.insertAfterCurrent" = true;
-        "browser.startup.homepage" = "http://odp-1:5678";
-        "gwfox.plus" = true;
         "sidebar.animation.enabled" = false;
         "svg.context-properties.content.enabled" = true;
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
@@ -31,6 +59,9 @@ in
         "widget.windows.mica.extra" = true;
         "widget.windows.mica.popups" = 2;
         "widget.windows.mica.toplevel-backdrop" = 2;
+        # FIXME: These don't apply.
+        # "sidebar.verticalTabs" = true;
+        # "sidebar.verticalTabs.dragToPinPromo.dismissed" = true;
       };
     };
 
@@ -39,8 +70,13 @@ in
       firefoxTheme = {
         deps = [ "specialfs" ];
         text = ''
-          export PROFILE=$(cat /home/${config.defaultUser}/.mozilla/firefox/profiles.ini | grep Path | cut -d= -f2)
-          ln -sfn ${gwfox}/chrome /home/${config.defaultUser}/.mozilla/firefox/''${PROFILE}/chrome
+          mkdir -p /home/${config.defaultUser}/.mozilla/firefox \
+            && chown -R ${config.defaultUser} /home/${config.defaultUser}/.mozilla/firefox
+
+          if [[ -n /home/${config.defaultUser}/.mozilla/firefox/profiles.ini ]]; then
+            PROFILE=$(grep "Path=.*.default" /home/${config.defaultUser}/.mozilla/firefox/profiles.ini | cut -d= -f2)
+            ln -sfn ${gwfox}/chrome /home/${config.defaultUser}/.mozilla/firefox/''${PROFILE}/chrome
+          fi
         '';
       };
     };
