cae7e955a2b1994fea17c476772ddfe9ba8a316a
detroyejr@outlook.com <detroyejr@outlook.com>
2026-06-05 10:46:19 -0400

    sops: tweak key file location

--- modules/default.nix
+++ modules/default.nix
@@ -160,7 +160,7 @@
       defaultSopsFile = ../secrets/secrets.yaml;
       age = {
         sshKeyPaths = [ "/home/detroyejr/.ssh/main_server_ed25519" ];
-        keyFile = "/home/detroyejr/.config/sops/age/keys.txt";
+        keyFile = "/etc/xdg/sops/age/keys.txt";
         generateKey = true;
       };
     };
