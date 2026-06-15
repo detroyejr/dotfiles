60fe179416b4d00bc0372c9ed6f540242058601a
detroyejr <detroyejr@outlook.com>
2026-06-15 10:01:29 -0400

    prometheus: fewer jobs defined

--- modules/services/prometheus.nix
+++ modules/services/prometheus.nix
@@ -15,61 +15,19 @@ in
 
     services.prometheus.scrapeConfigs = [
       {
-        job_name = "odp-1";
+        job_name = "odp";
         scrape_interval = "20s";
         static_configs = [
           {
-            targets = [ "odp-1:9100" ];
+            targets = map (x: "odp-${x}:9100") [
+              "1"
+              "2"
+              "3"
+              "4"
+              "5"
+            ];
             labels = {
-              alias = "odp-1";
-            };
-          }
-        ];
-      }
-      {
-        job_name = "odp-2";
-        scrape_interval = "20s";
-        static_configs = [
-          {
-            targets = [ "odp-2:9100" ];
-            labels = {
-              alias = "odp-2";
-            };
-          }
-        ];
-      }
-      {
-        job_name = "odp-3";
-        scrape_interval = "20s";
-        static_configs = [
-          {
-            targets = [ "odp-3:9100" ];
-            labels = {
-              alias = "odp-3";
-            };
-          }
-        ];
-      }
-      {
-        job_name = "odp-4";
-        scrape_interval = "20s";
-        static_configs = [
-          {
-            targets = [ "odp-4:9100" ];
-            labels = {
-              alias = "odp-4";
-            };
-          }
-        ];
-      }
-      {
-        job_name = "odp-5";
-        scrape_interval = "20s";
-        static_configs = [
-          {
-            targets = [ "odp-5:9100" ];
-            labels = {
-              alias = "odp-5";
+              alias = "odp";
             };
           }
         ];
