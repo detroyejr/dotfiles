bf8eddf2ae0385639c718033a0bdf5a5969226fe
Jonathan De Troye <detroyejr@outlook.com>
2026-08-19 12:20:40 -0400

    omarchy: tweak sensitivity for mouse but not trackpad

--- dotfiles/omarchy/hypr/input.lua
+++ dotfiles/omarchy/hypr/input.lua
@@ -3,9 +3,13 @@ o.exec_on_start("hyprctl setcursor Bibata-Modern-Ice 14")
 
 hl.config({
   input =  {
-    sensitivity = -0.5,
     touchpad = {
       natural_scroll = true
     }
   }
+})
+
+hl.device({
+  name = "steelseries-steelseries-sensei-310-esports-mouse",
+  sensitivity = -0.5,
 })
