5cf81f0704d356de92155f87e6a74b2010774229
Jonathan De Troye <detroyejr@outlook.com>
2026-08-20 11:11:17 -0400

    omarchy: set the preferred mode of Dell monitor

--- dotfiles/omarchy/hypr/monitors.lua
+++ dotfiles/omarchy/hypr/monitors.lua
@@ -6,5 +6,5 @@ local omarchy_gdk_scale = 1
 hl.env("GDK_SCALE", tostring(omarchy_gdk_scale))
 hl.monitor({ output = "", mode = "preferred", position = "auto", scale = "auto" })
 
-hl.monitor({ output = "desc:Dell Inc. DELL S2721QS BVR9513", mode = "preferred", position = "auto", scale = 1.5 })
+hl.monitor({ output = "desc:Dell Inc. DELL S2721QS BVR9513", mode = "2560x1440@59.95Hz", position = "auto", scale = 1.5 })
 hl.monitor({ output = "eDP-1", mode = "preferred", position = "auto", scale = 1.5 })
