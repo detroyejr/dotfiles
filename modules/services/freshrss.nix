46af449ee9bbd4d1177144399e77d73b65cf754c
detroyejr <detroyejr@outlook.com>
2026-09-08 10:20:36 -0400

    freshrss: add llm classification

--- modules/services/freshrss.nix
+++ modules/services/freshrss.nix
@@ -1,10 +1,39 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 let
   cfg = config.services.freshrss;
+
+  llmClassification = pkgs.stdenvNoCC.mkDerivation {
+    pname = "freshrss-extension-llm-classification";
+    version = "0.3";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "FreshRSS";
+      repo = "Extensions";
+      rev = "30deb1664311febf9efb610a5db7c8fc1f9f86f8";
+      hash = "sha256-0PxFDgHdF6GBh8dNdG4ga5ogmVb2cfW+mvuTVU8j3QA=";
+    };
+
+    sourceRoot = "source/xExtension-LlmClassification";
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p "$out/share/freshrss/extensions/xExtension-LlmClassification"
+      cp -r . "$out/share/freshrss/extensions/xExtension-LlmClassification/"
+      runHook postInstall
+    '';
+
+    meta = with pkgs.lib; {
+      description = "FreshRSS extension that classifies articles with an OpenAI-compatible LLM API";
+      homepage = "https://github.com/FreshRSS/Extensions/tree/main/xExtension-LlmClassification";
+      license = licenses.agpl3Only;
+      platforms = platforms.all;
+    };
+  };
 in
 {
   config = lib.mkIf cfg.enable {
@@ -29,6 +58,8 @@ in
       baseUrl = "http://odp-1";
       dataDir = "/var/lib/freshrss";
       passwordFile = config.sops.secrets."freshrss/password".path;
+
+      extensions = [ llmClassification ];
 
       api.enable = true;
     };
