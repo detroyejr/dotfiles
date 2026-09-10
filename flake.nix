1932012b4770c6ba3acf15339ca3f4fcce29eec1
detroyejr <detroyejr@outlook.com>
2026-09-10 13:46:10 -0400

    nixos-hardware: follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixos-hardware = {
+      url = "github:NixOS/nixos-hardware/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     jovian = {
       url = "github:Jovian-Experiments/Jovian-NixOS";
